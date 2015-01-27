19ccc814a1861b104c9ba37b7d27623aad58cf42
jbphet <john.blanco@colorado.edu>
2015-01-27 14:37:37 -0700

    Started setting up an initial page for selection of simulation and target language.

--- index.js
+++ index.js
@@ -4,69 +4,33 @@
  * Main entry point for PhET translation web app.
  */
 
+// modules
 var express = require( 'express' );
 var https = require( 'https' );
-var app = express();
 var doT = require( 'express-dot' );
-var pub = __dirname + '/public';
+
+// constants
+var app = express();
 var view = __dirname + '/views';
+var LISTEN_PORT = 16372;
 
-app.configure( function() {
-  app.set( 'views', __dirname + '/html/views' );
-  app.set( 'view engine', 'dot' );
-  app.engine( 'html', doT.__express );
-  app.use( '/translate/css', express.static( __dirname + '/public/css' ) );
-  app.use( '/translate/sim/css', express.static( __dirname + '/public/css' ) );
-  app.use( '/translate/img', express.static( __dirname + '/public/img' ) );
-  app.use( '/translate/js', express.static( __dirname + '/public/js' ) );
-  app.use( '/translate/sim/js', express.static( __dirname + '/public/js' ) );
-  app.use( '/translate/fonts', express.static( __dirname + '/public/fonts' ) );
-} );
+// Configure the ExpressJS app
+app.set( 'views', __dirname + '/html/views' );
+app.set( 'view engine', 'dot' );
+app.engine( 'html', doT.__express );
+app.use( '/translate/css', express.static( __dirname + '/public/css' ) );
+app.use( '/translate/sim/css', express.static( __dirname + '/public/css' ) );
+app.use( '/translate/img', express.static( __dirname + '/public/img' ) );
+app.use( '/translate/js', express.static( __dirname + '/public/js' ) );
+app.use( '/translate/sim/js', express.static( __dirname + '/public/js' ) );
+app.use( '/translate/fonts', express.static( __dirname + '/public/fonts' ) );
 
-app.get( '/', function( req, res ) {
-  var templateData = { title: "This title is from the template data JSON object" };
-  res.render( 'index.html', templateData );
-} );
-
-app.get( '/nolayout', function( req, res ) {
-  var templateData = { title: "This title is from the template data JSON object", layout: false };
-  res.render( 'index.html', templateData );
-} );
+//----------------------------------------------------------------------------
+// Set up the routes
+//----------------------------------------------------------------------------
 
 app.get( '/translate/', function( req, res ) {
-
-  // Pull the string data from github.
-  https.get( {
-    host: 'raw.githubusercontent.com',
-    path: '/phetsims/chains/master/strings/chains-strings_en.json'
-  }, function( response ) {
-    response.on( 'data', function( data ) {
-
-      // Parse the returned JSON data into a JavaScript object.
-      strings = JSON.parse( data );
-
-      // Create a table for the strings to be translated.
-      var stringsTable = '';
-      for ( var key in strings ) {
-        if ( strings.hasOwnProperty( key ) ) {
-          stringsTable += '<tr>';
-          stringsTable += '<td>' + strings[ key ] + '</td>';
-          stringsTable += '<td><input type=\"text\" </td>';
-          stringsTable += '</tr>';
-        }
-      }
-
-      // Assemble the data that will be supplied to the template.
-      var templateData = {
-        title: "Enter a translation for each English string:",
-        destinationLanguage: 'Spanish',
-        stringsTable: stringsTable
-      };
-
-      // Render the page.
-      res.render( 'index.html', templateData );
-    } );
-  } );
+  res.sendfile( __dirname + '/html/views/translate-home.html' );
 } );
 
 function escapeHTML( s ) {
@@ -119,4 +83,9 @@ app.get( '/translate/sim/:simName', function( req, res ) {
   } );
 } );
 
-app.listen( 16372 );
+app.get( '/*', function( req, res ) {
+  console.log( 'req.baseUrl = ' + req.baseUrl );
+  res.send( '<p>Error: Page not found.  URL = ' + req.baseUrl + '</p>' );
+} );
+
+app.listen( LISTEN_PORT, function() { console.log( 'Listening on port ' + LISTEN_PORT ) } );
