d5228943bcf05dd501868ebf38354b43beb54fed
jbphet <john.blanco@colorado.edu>
2015-02-11 15:09:19 -0700

    Improved code for logging cookie data.

--- rosetta.js
+++ rosetta.js
@@ -47,7 +47,7 @@ app.use( express.cookieParser() );
 if ( REQUIRE_LOGIN ) {
   app.get( '/translate/*', function( req, res, next ) {
     console.log( 'Checking for login cookie' );
-    console.log( 'all cookies = ' + req.cookies );
+    console.log( 'all cookies = ' + JSON.stringify( req.cookies ) );
     // check if client sent cookie
     var cookie = req.cookies.JSESSIONID;
     if ( cookie === undefined ) {
