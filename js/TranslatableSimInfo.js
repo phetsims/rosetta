ac06be5b261a8df09ee5dc9968a2916b2c90b0fc
jbphet <john.blanco@colorado.edu>
2015-02-10 17:57:54 -0700

    Added a set of sims that have been published since the new string query param has been working.

--- js/TranslatableSimInfo.js
+++ js/TranslatableSimInfo.js
@@ -12,20 +12,26 @@
 'use strict'; // TODO: Does this work, and is it needed?
 
 var simInfoArray = [
-  {
-    simTitle: 'Reactants, Products, and Leftovers',
-    projectName: 'reactants-products-and-leftovers',
-    testUrl: 'http://phet.colorado.edu/sims/html/reactants-products-and-leftovers/latest/reactants-products-and-leftovers_en.html'
-  },
+  // please list sims in alphabetical order
   {
     simTitle: 'Arithmetic',
     projectName: 'arithmetic',
-    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/arithmetic/1.0.0-dev.12/arithmetic_en.html'
+    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/arithmetic/1.0.0-dev.13/arithmetic_en.html'
   },
   {
-    simTitle: 'Neuron',
-    projectName: 'neuron',
-    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/neuron/1.0.0-dev.7/neuron_en.html'
+    simTitle: 'Example Sim',
+    projectName: 'example-sim',
+    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/example-sim/1.0.0-dev.5/example-sim_en.html'
+  },
+  {
+    simTitle: 'Molecules and Light',
+    projectName: 'molecules-and-light',
+    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/molecules-and-light/1.0.0-dev.4/molecules-and-light_en.html'
+  },
+  {
+    simTitle: 'Molecule Shapes',
+    projectName: 'molecule-shapes',
+    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/molecule-shapes/1.0.0-dev.15/molecule-shapes_en.html'
   }
 ];
 
