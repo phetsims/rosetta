7e64d943208413c8050cb27143c3c30bc488d47b
Brian Newsom <brian.newsom@colorado.edu>
2015-03-24 15:52:11 -0600

    Move TranslatableSimInfo into JSON file, see #5

--- js/TranslatableSimInfo.js
+++ js/TranslatableSimInfo.js
@@ -13,29 +13,8 @@
  */
 'use strict'; // TODO: Does this work, and is it needed?
 
-var simInfoArray = [
-  // please list sims in alphabetical order
-  {
-    simTitle: 'Arithmetic',
-    projectName: 'arithmetic',
-    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/arithmetic/1.0.0-dev.13/arithmetic_en.html'
-  },
-  {
-    simTitle: 'Example Sim',
-    projectName: 'example-sim',
-    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/example-sim/1.0.0-dev.5/example-sim_en.html'
-  },
-  {
-    simTitle: 'Molecules and Light',
-    projectName: 'molecules-and-light',
-    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/molecules-and-light/1.0.0-dev.4/molecules-and-light_en.html'
-  },
-  {
-    simTitle: 'Molecule Shapes',
-    projectName: 'molecule-shapes',
-    testUrl: 'http://www.colorado.edu/physics/phet/dev/html/molecule-shapes/1.0.0-dev.15/molecule-shapes_en.html'
-  }
-];
+// Keep data in simInfoArray.json in alphabetical order
+var simInfoArray = require( '../data/simInfoArray.json' );
 
 module.exports.simInfoArray = simInfoArray;
 
