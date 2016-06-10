9fd2d0a92197ccb8ca9315aee9a00df89bf4828f
jbphet <john.blanco@colorado.edu>
2016-06-10 17:12:31 -0600

    temporary commit to allow testing of translation utility while it looks off line to other users

--- rosetta.js
+++ rosetta.js
@@ -149,20 +149,20 @@ if ( !ENABLED ){
 }
 
 // route that checks whether the user is logged in
-app.get( '/translate*', routes.checkForValidSession );
+app.get( '/test-translate*', routes.checkForValidSession );
 
 // landing page for the translation utility
-app.get( '/translate', routes.chooseSimulationAndLanguage );
+app.get( '/test-translate', routes.chooseSimulationAndLanguage );
 
 // route for translating a specific sim to a specific language
-app.get( '/translate/sim/:simName?/:targetLocale?', routes.translateSimulation );
-app.post( '/translate/sim/save/:simName?/:targetLocale?', routes.saveStrings );
-app.post( '/translate/sim/:simName?/:targetLocale?', routes.submitStrings );
+app.get( '/test-translate/sim/:simName?/:targetLocale?', routes.translateSimulation );
+app.post( '/test-translate/sim/save/:simName?/:targetLocale?', routes.saveStrings );
+app.post( '/test-translate/sim/:simName?/:targetLocale?', routes.submitStrings );
 
 // route for extracting strings from a sim
-app.get( '/translate/extractStrings', routes.extractStringsAPI );
+app.get( '/test-translate/extractStrings', routes.extractStringsAPI );
 
-app.get( '/translate/logout', routes.logout );
+app.get( '/test-translate/logout', routes.logout );
 
 // fall through route
 app.get( '/*', routes.pageNotFound );
