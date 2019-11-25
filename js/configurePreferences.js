c7b66670e37cf213018c37ddf10fb3fedc5ddae0
jbphet <john.blanco@colorado.edu>
2019-11-25 13:11:10 -0700

    steps towards improved DB interaction and code, see #167 and #208

--- js/configurePreferences.js
+++ js/configurePreferences.js
@@ -1,7 +1,7 @@
 // Copyright 2018-2019, University of Colorado Boulder
 
 /**
- * Compile and validate a preferences object to be used throughout the app.
+ * Interpret and validate a preferences object that will be used throughout the app.
  *
  * @author Michael Kauzmann (PhET Interactive Simulations)
  * @author John Blanco (PhET Interactive Simulations)
@@ -9,51 +9,60 @@
 
 'use strict';
 
+// modules
 const assert = require( 'assert' );
-const fs = require( 'fs' );
 const { Client } = require( 'pg' ); //eslint-disable-line
+const fs = require( 'fs' );
+const passwdUser = require( 'passwd-user' ); // eslint-disable-line require-statement-match
 const winston = require( 'winston' );
+
+// constants
+const PREFERENCES_FILE_PATH_FROM_DIR = '/.phet/build-local.json';
 
 module.exports = async function() {
 
-  let PREFERENCES_FILE;
-
   /**
-   * Rosetta is run under user "phet-admin" on the dev and production servers. However, "process.env.HOME" will get the
-   * home directory of the user who is starting the process, not phet-admin's home directory, therefore we need to use
-   * the following approach to get the home directory.
+   * Locate and read the preferences file.
    */
-  if ( !/^win/.test( process.platform ) ) {
-    const passwdUser = require( 'passwd-user' ); // eslint-disable-line require-statement-match
-    PREFERENCES_FILE = passwdUser.sync( process.getuid() ).homedir + '/.phet/build-local.json';
+  let preferencesFileName;
+  const platform = process.platform;
+  winston.log( 'info', 'platform = ' + platform );
+  if ( platform.indexOf( 'win' ) === 0 ) {
+
+    // windows
+    preferencesFileName = process.env.HOME + PREFERENCES_FILE_PATH_FROM_DIR;
   }
   else {
-    PREFERENCES_FILE = process.env.HOME + '/.phet/build-local.json';
+
+    // The following somewhat odd looking code gets the preferences file from the home directory when running under a
+    // Unix variant.  This was necessary because rosetta is generally run under phet-admin, but "process.env.HOME" was
+    // returning the home directory of the user who is starting the process, not phet-admin's home directory.
+    preferencesFileName = passwdUser.sync( process.getuid() ).homedir + PREFERENCES_FILE_PATH_FROM_DIR;
   }
 
-  winston.log( 'info', 'obtaining configuration information from ' + PREFERENCES_FILE );
+  winston.log( 'info', 'preferences file full path and name: ' + preferencesFileName );
 
-  // ensure that the preferences file exists and has the required fields
-  assert( fs.existsSync( PREFERENCES_FILE ), 'missing preferences file ' + PREFERENCES_FILE );
-  const preferences = require( PREFERENCES_FILE );
-  assert( preferences.githubUsername, 'githubUsername is missing from ' + PREFERENCES_FILE );
-  assert( preferences.githubPassword, 'githubPassword is missing from ' + PREFERENCES_FILE );
-  assert( preferences.buildServerAuthorizationCode, 'buildServerAuthorizationCode is missing from ' + PREFERENCES_FILE );
-  assert( preferences.serverToken, 'serverToken is missing from ' + PREFERENCES_FILE );
-  assert( preferences.rosettaSessionSecret, 'rosettaSessionSecret is missing from ' + PREFERENCES_FILE +
+  // ensure that the preferences file exists
+  assert( fs.existsSync( preferencesFileName ), 'preferences file not found: ' + preferencesFileName );
+
+  // read the preferences file and verify that the needed information is present
+  const preferencesJSON = fs.readFileSync( preferencesFileName );
+  const preferences = JSON.parse( preferencesJSON );
+  assert( preferences.githubUsername, 'githubUsername is missing from ' + preferencesFileName );
+  assert( preferences.githubPassword, 'githubPassword is missing from ' + preferencesFileName );
+  assert( preferences.buildServerAuthorizationCode, 'buildServerAuthorizationCode is missing from ' + preferencesFileName );
+  assert( preferences.serverToken, 'serverToken is missing from ' + preferencesFileName );
+  assert( preferences.rosettaSessionSecret, 'rosettaSessionSecret is missing from ' + preferencesFileName +
                                             '. To set this up for local testing add any string as the value for "rosettaSessionSecret"' );
 
-  preferences.productionServerURL = preferences.productionServerURL || 'https://phet.colorado.edu';
-  preferences.productionServerName = preferences.productionServerName || 'phet-server.int.colorado.edu';
-
   // verify that the information necessary for connecting to the short-term-string-storage database is available
-  assert( preferences.rosettaDbHost, `rosettaDbHost is missing from ${PREFERENCES_FILE}` );
-  assert( preferences.rosettaDbPort, `rosettaDbPort is missing from ${PREFERENCES_FILE}` );
-  assert( preferences.rosettaDbName, `rosettaDbName is missing from ${PREFERENCES_FILE}` );
-  assert( preferences.rosettaDbUser, `rosettaDbUser is missing from ${PREFERENCES_FILE}` );
-  assert( preferences.rosettaDbPass, `rosettaDbPass is missing from ${PREFERENCES_FILE}` );
+  assert( preferences.rosettaDbHost, `rosettaDbHost is missing from ${preferencesFileName}` );
+  assert( preferences.rosettaDbPort, `rosettaDbPort is missing from ${preferencesFileName}` );
+  assert( preferences.rosettaDbName, `rosettaDbName is missing from ${preferencesFileName}` );
+  assert( preferences.rosettaDbUser, `rosettaDbUser is missing from ${preferencesFileName}` );
+  assert( preferences.rosettaDbPass, `rosettaDbPass is missing from ${preferencesFileName}` );
 
-  // set up the environment variables used by the 'pg' module to interact with the DB
+  // set up the environment variables used by the 'pg' module to interact with the DB used for short-term string storage
   process.env = Object.assign( process.env, {
     PGUSER: preferences.rosettaDbUser,
     PGHOST: preferences.rosettaDbHost,
@@ -62,8 +71,12 @@ module.exports = async function() {
     PGPORT: preferences.rosettaDbPort
   } );
 
+  // get information on the server or use defaults
+  preferences.productionServerURL = preferences.productionServerURL || 'https://phet.colorado.edu';
+  preferences.productionServerName = preferences.productionServerName || 'phet-server.int.colorado.edu';
+
   // check that the DB server is running and that a connection can be successfully established
-  winston.log( 'info', 'testing database connection' );
+  winston.log( 'info', 'testing database connection...' );
   const client = new Client();
   try {
     await client.connect();
