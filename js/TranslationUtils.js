cf9f71caae1e52c40e2a260745f5a6746cf51714
phet-dev <phetadmin@gmail.com>
2020-02-27 03:46:28 -0700

    update copyright dates from daily grunt work

--- js/TranslationUtils.js
+++ js/TranslationUtils.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * This file holds various utilities useful for translation.
--- js/routeHandlers.js
+++ js/routeHandlers.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * Handler functions for ExpressJS-style routes that exist in the PhET translation utility.
