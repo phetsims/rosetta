65efbfb92a61e4289201f384cedcb858dc728cd2
Liam Mulhall <muedli.liam@gmail.com>
2020-08-11 16:02:31 -0600

    Working on getting rid of jQuery. Part of https://github.com/phetsims/rosetta/issues/238.

--- js/testHarnessClient.js
+++ js/testHarnessClient.js
@@ -1,25 +1,57 @@
 // Copyright 2018-2020, University of Colorado Boulder
 
 /**
- * JavaScript that implements the test harness behavior, which is used to debug and verify the PhET translation utility.
- * This code runs on the client and generally stimulates the server by sending URLs to it.
+ * Client-side JavaScript that makes it so that a PhET team member can perform the tests specified on the test page.
+ *
+ * @author Aaron Davis
+ * @author Liam Mulhall
  */
 
 'use strict';
 
+// TODO: Finish rewriting the old code below.
+// document.addEventListener( "DOMContentLoaded", () => {
+//
+//   // Get all buttons on the page.
+//   const buttons = document.getElementsByTagName( 'button' );
+//
+//   // Form the beginning of the runSpecificTest route.
+//   const runSpecificTestUrl = window.location.href.replace( 'test', 'runSpecificTest' );
+//
+//   // Make it so that when a button is clicked, the test associated with the button is requested.
+//   for ( let button of buttons ) {
+//
+//     // The button's ID should be the testID for the runSpecificTest route.
+//     const testID = button.getAttribute( 'id' );
+//     button.addEventListener( 'click', () => {
+//       fetch( `${runSpecificTestUrl}/${testID}` )
+//         .then( data => {
+//           console.log( data );
+//         } )
+//         .catch( error => {
+//           console.log( `Fetch for ${runSpecificTestUrl}/${testID} failed. Error: ${error}.` )
+//         } );
+//     } );
+//   }
+// } );
+
 $( document ).ready( function() {
 
-  const runTestRUL = window.location.href.replace( 'test', 'runTest' ) + '/';
+  // runTest is incorrect. It should be runSpecificTest. I think this is my fault.
+  // (I think I renamed runTest to runSpecificTest and didn't realize it would cause this problem.)
+  const runTestUrl = window.location.href.replace( 'test', 'runTest' ) + '/';
 
   // add a handler for each button that will make it so that clicking that button will request the associated test
   const buttons = document.getElementsByTagName( 'button' );
-  for ( let i = 0; i < buttons.length; i++ ){
+  for ( let i = 0; i < buttons.length; i++ ) {
     const button = buttons[ i ];
-    (function(){
-      const id = button.getAttribute('id');
-      button.addEventListener( 'click', function(){
-        $.get( runTestRUL + id );
+    ( function() {
+      const id = button.getAttribute( 'id' );
+      button.addEventListener( 'click', function() {
+        $.get( runTestUrl + id, function( data ) {
+          console.log( data );
+        } );
       } );
-    })();
+    } )();
   }
 } );
