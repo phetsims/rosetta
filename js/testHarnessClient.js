77ab3272555ceb7f5c2e96db32ffbe4a75166dd8
Liam Mulhall <muedli.liam@gmail.com>
2020-08-17 14:27:48 -0600

    Finished rewriting old jQuery code. See https://github.com/phetsims/rosetta/issues/238.

--- js/testHarnessClient.js
+++ js/testHarnessClient.js
@@ -9,55 +9,37 @@
 
 'use strict';
 
-// Modules
-// const nodeFetch = require('node-fetch');
+/*global fetch:false*/
 
-// // TODO: Finish rewriting the old code below.
-// document.addEventListener( "DOMContentLoaded", () => {
-//
-//   // Get all buttons on the page.
-//   const buttons = document.getElementsByTagName( 'button' );
-//
-//   // Form the beginning of the runSpecificTest route.
-//   const runSpecificTestUrl = window.location.href.replace( 'test', 'runSpecificTest' );
-//   console.log(runSpecificTestUrl);
-//
-//   // Make it so that when a button is clicked, the test associated with the button is requested.
-//   for ( const button of buttons ) {
-//
-//     // The button's ID should be the testID for the runSpecificTest route.
-//     const testID = button.getAttribute( 'id' );
-//     button.addEventListener( 'click', () => {
-//
-//       // TODO: Use node-fetch as in localeInfo.js line 39.
-//       fetch( runSpecificTestUrl + testID )
-//         .then( data => {
-//           console.log( data );
-//         } )
-//         .catch( error => {
-//           console.log( `Fetch for ${runSpecificTestUrl + testID} failed. Error: ${error}.` );
-//         } );
-//     } );
-//   }
-// } );
+// TODO: Finish rewriting the old code below.
+document.addEventListener( 'DOMContentLoaded', () => {
 
-$( document ).ready( function() {
-
-  // runTest is incorrect. It should be runSpecificTest. I think this is my fault.
-  // (I think I renamed runTest to runSpecificTest and didn't realize it would cause this problem.)
-  const runTestUrl = window.location.href.replace( 'test', 'runTest' ) + '/';
-
-  // add a handler for each button that will make it so that clicking that button will request the associated test
+  // Get all buttons on the page.
   const buttons = document.getElementsByTagName( 'button' );
-  for ( let i = 0; i < buttons.length; i++ ) {
-    const button = buttons[ i ];
-    ( function() {
-      const id = button.getAttribute( 'id' );
-      button.addEventListener( 'click', function() {
-        $.get( runTestUrl + id, function( data ) {
+
+  // Form the beginning of the runSpecificTest route.
+  const runSpecificTestUrl = window.location.href.replace( 'test', 'runSpecificTest' );
+  console.log(runSpecificTestUrl);
+
+  // Make it so that when a button is clicked, the test associated with the button is requested.
+  for ( const button of buttons ) {
+
+    // The button's ID should be the testID for the runSpecificTest route.
+    const testID = button.getAttribute( 'id' );
+    button.addEventListener( 'click', () => {
+
+      // It seems I can't use fetch (basically new and improved XHR) because Grunt complains about it.
+      // I can't use node-fetch because the browser complains about it. (Require isn't defined.)
+      // I'd like to be able to use fetch, but I might try using XMLHttpRequest instead.
+      const fullTestUrl = runSpecificTestUrl + '/' + testID;
+      fetch( fullTestUrl )
+        .then( data => {
+          console.log( `URL: ${fullTestUrl}.` );
           console.log( data );
+        } )
+        .catch( error => {
+          console.log( `Fetch for ${fullTestUrl} failed. Error: ${error}.` );
         } );
-      } );
-    } )();
+    } );
   }
 } );
