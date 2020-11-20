870def6f3baeaa0d01b179e5538eb3386cf7eccf
Liam Mulhall <muedli.liam@gmail.com>
2020-11-19 21:56:24 -0700

    More work on getting rid of jQuery. See https://github.com/phetsims/rosetta/issues/238.

--- js/translateSim.js
+++ js/translateSim.js
@@ -11,15 +11,27 @@
 // const DOUBLE_BRACE_PATTERN_REGULAR_EXP = /\{\{\w+\}\}/g;
 
 //===========================================================================//
+// Define helper functions.                                                  //
+//===========================================================================//
+
+// function rowIsValid() {
+//   console.log( 'Hello, rowIsValid!' );
+// }
+//
+// function patternsAreValid() {
+//   console.log( 'Hello, patternsAreValid!' );
+// }
+
+//===========================================================================//
 // Define functions for use once the DOM is loaded.                          //
 //===========================================================================//
 
 function testTranslation() {
-  console.log( 'Hello, world!' );
+  console.log( 'Hello, testTranslation!' );
 }
 
 function saveTranslation() {
-  console.log( 'Hello, world!' );
+  console.log( 'Hello, saveTranslation!' );
 }
 
 //===========================================================================//
@@ -28,19 +40,35 @@ function saveTranslation() {
 
 document.addEventListener( 'DOMContentLoaded', () => {
 
-  // Create a list of the top and bottom test buttons.
+  // Create lists of buttons.
   const testButtonsList = document.getElementsByClassName( 'test-btn' );
-
-  // Create a list of the top and bottom save buttons.
   const saveButtonsList = document.getElementsByClassName( 'save-btn' );
 
-  // Add event listeners for the test buttons.
+  // Add functionality for the test buttons.
   for ( const testButton of testButtonsList ) {
     testButton.addEventListener( 'click', testTranslation );
   }
 
-  // Add event listeners for the save buttons.
+  // Add functionality for the save buttons.
   for ( const saveButton of saveButtonsList ) {
     saveButton.addEventListener( 'click', saveTranslation );
+  }
+
+  // Create a list of the user inputs.
+  const userInputsSelector = '.string-translation-table div[contenteditable]';
+  const userInputsList = document.querySelectorAll( userInputsSelector );
+
+  // Get the sim data.
+  const simData = document.getElementById( 'simData' );
+
+  // Set a boolean for whether the data direction is right-to-left.
+  const dataDirectionIsRtl = simData.getAttribute( 'data-direction' ) === 'rtl';
+
+  // Change the direction of the text if the data direction is right-to-left.
+  if ( dataDirectionIsRtl ) {
+    for ( const userInput of userInputsList ) {
+      userInput.setAttribute( 'dir', 'rtl' );
+      userInput.style.textAlign = 'right';
+    }
   }
 } );
