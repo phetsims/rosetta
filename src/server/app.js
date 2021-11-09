2971d28206532adacaec50a8ad07000a597a2906
Liam Edward Mulhall <mulhall.edward.liam@gmail.com>
2021-11-09 11:46:28 -0700

    Add dotenv Config

--- src/server/app.js
+++ src/server/app.js
@@ -1,16 +1,18 @@
 // Copyright 2021, University of Colorado Boulder
 
+// packages
 const express = require( 'express' );
 const path = require( 'path' );
 
-const PORT = 3000;
-const STATIC_ASSETS_PATH = path.join( __dirname, '..', '..', 'static' );
+// server modules
+const config = require( './config.js' );
 
 const app = express();
 
-app.use( express.static( STATIC_ASSETS_PATH ) );
+const staticAssetsPath = path.join( __dirname, '..', '..', 'static' );
+app.use( express.static( staticAssetsPath ) );
 app.get( '/translate', ( req, res ) => {
   res.sendFile( path.join( __dirname, '..', '..', 'static', 'index.html' ) );
 } );
 
-app.listen( PORT, () => console.log( `http://localhost:${PORT}` ) );
+app.listen( config.SERVER_PORT, () => console.log( `http://localhost:${config.SERVER_PORT}/translate` ) );
