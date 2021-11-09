168690cb1cccf692f3ce761e7f8a4aa49cc996d7
Liam Edward Mulhall <mulhall.edward.liam@gmail.com>
2021-11-08 20:11:44 -0700

    Use React for Frontend

---
+++ src/client/components/Rosetta.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Rosetta() {
+  return <h1>PhET Translation Tool</h1>;
+};
---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Rosetta from './components/Rosetta.js';
+
+ReactDOM.render( <Rosetta/>, document.getElementById( 'root' ) );
