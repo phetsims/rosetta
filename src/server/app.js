fab696859444571bb46597c2c2a2f6c92d2ca282
Liam Edward Mulhall <mulhall.edward.liam@gmail.com>
2021-11-08 20:11:44 -0700

    Use React for Frontend

---
+++ src/server/app.js
@@ -0,0 +1,23 @@
+const express = require( 'express' );
+const path = require( 'path' );
+
+const PORT = 3000;
+const STATIC_ASSETS_PATH = path.join( __dirname, '..', '..', 'static' );
+
+const app = express();
+
+app.use( '/static', express.static( STATIC_ASSETS_PATH ) );
+app.get( '/', ( req, res ) => {
+  const html = `
+    <!doctype html>
+    <html lang="en">
+    	<body>
+    		<div id="root"></div>
+    		<script src="/static/bundle.js"></script>
+    	</body>
+    </html>
+  `;
+  res.send( html );
+} );
+
+app.listen( PORT, () => console.log( `http://localhost:${PORT}` ) );
