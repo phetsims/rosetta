b37cab25dff58ba77ddded4425d2758251a5ceda
Liam Edward Mulhall <mulhall.edward.liam@gmail.com>
2021-11-10 13:36:12 -0700

    Add Tests for Existing API Funcs
    Since the API funcs are the crux of Rosetta, tests for those
    funcs seems prudent.
    
    In this commit we add simple tests for `localeInfo` and
    `simNames`.

---
+++ test/localeInfo.test.js
@@ -0,0 +1,10 @@
+// Copyright 2021, University of Colorado Boulder
+
+import getLocaleInfo from '../src/server/getLocaleInfo.js';
+
+QUnit.module( 'localeInfo' );
+
+QUnit.test( 'getLocaleInfo should return truthy', async assert => {
+  const localeInfo = await getLocaleInfo();
+  assert.ok( localeInfo );
+} );
