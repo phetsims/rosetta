466926a4fcfa2111125b6d695b8c841d8c9e5de0
Liam Edward Mulhall <mulhall.edward.liam@gmail.com>
2021-11-10 13:17:44 -0700

    Add Funcs to Get Sim Metadata and Sim Names

---
+++ src/server/getSimMetadata.js
@@ -0,0 +1,29 @@
+// Copyright 2021, University of Colorado Boulder
+
+import axios from 'axios';
+import config from './config.js';
+import logger from './logger.js';
+
+const METADATA_URL = config.SERVER_URL +
+                     '/services/metadata/1.2/simulations?format=json&type=html&include-unpublished=true&summary';
+const METADATA_REQ_OPTIONS = {
+  auth: {
+    username: 'token',
+    password: config.SERVER_TOKEN
+  }
+};
+
+const getSimMetadata = async () => {
+  let simMetadata;
+  try {
+    const simMetadataRes = await axios.get( METADATA_URL, METADATA_REQ_OPTIONS );
+    simMetadata = simMetadataRes.data;
+  }
+  catch( e ) {
+    logger.error( e );
+  }
+  logger.info( 'got sim metadata; returning it' );
+  return simMetadata;
+};
+
+export { getSimMetadata as default };
---
+++ src/server/getSimNames.js
@@ -0,0 +1,25 @@
+// Copyright 2021, University of Colorado Boulder
+
+import getSimMetadata from './getSimMetadata.js';
+import logger from './logger.js';
+
+const getSimNames = async () => {
+  const simNames = [];
+  try {
+    const simMetadata = await getSimMetadata();
+    for ( const project of simMetadata.projects ) {
+
+      // slice html/ off the project name
+      // e.g. turn html/acid-base-solutions into acid-base-solutions
+      const simName = project.name.slice( 5 );
+      simNames.push( simName );
+    }
+  }
+  catch( e ) {
+    logger.error( e );
+  }
+  logger.info( 'got sim names; returning them' );
+  return simNames;
+};
+
+export { getSimNames as default };
