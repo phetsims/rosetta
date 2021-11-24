7b9d83da7230daa0621ae1effe536aeb5aabd3fe
Liam Mulhall <mulhall.edward.liam@gmail.com>
2021-11-24 01:18:11 -0700

    Add Dummy Component for Locale and Sim Form

---
+++ src/client/components/LocaleSelect.js
@@ -0,0 +1,15 @@
+// Copyright 2021, University of Colorado Boulder
+
+import React from 'react';
+
+const LocaleSelect = () => {
+  return (
+    <select>
+      <option>dummyOpt1</option>
+      <option>dummyOpt2</option>
+      <option>dummyOpt3</option>
+    </select>
+  );
+};
+
+export default LocaleSelect;
