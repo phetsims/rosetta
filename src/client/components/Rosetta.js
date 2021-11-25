95762fee970d1efe6c4f820ead2d747f8588cee5
Liam Mulhall <mulhall.edward.liam@gmail.com>
2021-11-24 22:10:56 -0700

    Finish Protecting Access to Rosetta

--- src/client/components/Rosetta.js
+++ src/client/components/Rosetta.js
@@ -18,12 +18,40 @@ function Rosetta() {
     }
   }, [] );
 
-  return (
-    <div>
-      <h1>PhET Translation Tool</h1>
-      {websiteUserData.loggedIn ? <RosettaRoutes/> : null}
-    </div>
-  );
+  let jsx;
+  const allowedAccess = websiteUserData.loggedIn && ( websiteUserData.trustedTranslator || websiteUserData.teamMember );
+  const notAllowedAccess = websiteUserData.loggedIn && ( !websiteUserData.trustedTranslator && !websiteUserData.teamMember );
+  if ( allowedAccess ) {
+    jsx = (
+      <div>
+        <h1>PhET Translation Tool</h1>
+        <RosettaRoutes/>
+      </div>
+    );
+  }
+  else if ( notAllowedAccess ) {
+    jsx = (
+      <div>
+        <h1>PhET Translation Tool</h1>
+        <p>
+          You need to be a trusted translator to access the PhET Translation Tool. To become a trusted translator,
+          please send an email to phethelp@gmail.com.
+        </p>
+      </div>
+    );
+  }
+  else if ( !websiteUserData.loggedIn ) {
+    jsx = (
+      <div>
+        <h1>PhET Translation Tool</h1>
+        <p>
+          You need to sign in to access the PhET Translation Tool.
+        </p>
+        <a href={`${window.location.origin}/en/sign-in?dest=${window.location.href}`}>Sign In</a>
+      </div>
+    );
+  }
+  return jsx;
 }
 
 export default Rosetta;
