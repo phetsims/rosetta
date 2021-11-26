ded24351cb1e8995cb786b6b232551f3667cbad6
Liam Mulhall <mulhall.edward.liam@gmail.com>
2021-11-25 17:42:42 -0700

    Refactor `onSubmit`

--- src/client/components/TranslationForm.js
+++ src/client/components/TranslationForm.js
@@ -28,47 +28,57 @@ const TranslationForm = () => {
 
   const navigate = useNavigate();
 
+  const save = async ( locale, sim, translation ) => {
+    const wantsToSave = window.confirm( `If you have a translation saved for ${params.sim} in locale ${params.locale}, it will be overwritten.` );
+    if ( wantsToSave ) {
+      try {
+        const postRes = await axios.post( '/translate/api/saveTranslation', translation );
+        console.log( postRes.data );
+        alert( 'Translation saved. Redirecting you to the PhET Translation Tool home page.' );
+        navigate( '/translate' );
+      }
+      catch( e ) {
+        console.error( e );
+      }
+    }
+  };
+
+  const submit = async ( locale, sim, translation ) => {
+    const wantsToSubmit = window.confirm( `Are you sure you want to submit your translation for ${params.sim} in locale ${params.locale}?` );
+    if ( wantsToSubmit ) {
+      try {
+        const postRes = await axios.post( '/translate/api/submitTranslation', translation );
+        console.log( postRes.data );
+        alert( 'Translation submitted. Your translation should appear on the PhET website in about half an hour. Redirecting you to the PhET Translation Tool home page.' );
+        navigate( '/translate' );
+      }
+      catch( e ) {
+        console.error( e );
+      }
+    }
+  };
+
+  const handleSubmit = async values => {
+    console.log( JSON.stringify( values, null, 2 ) );
+    const translation = {
+      userId: 123456,
+      sim: params.sim,
+      locale: params.locale,
+      translationFormData: values
+    };
+    if ( document.activeElement.dataset.flag === 'save' ) {
+      await save( params.locale, params.sim, translation );
+    }
+    else if ( document.activeElement.dataset.flag === 'submit' ) {
+      await submit( params.locale, params.sim, translation );
+    }
+  };
+
   return (
     <Formik
       enableReinitialize={true}
       initialValues={translationFormData}
-      onSubmit={values => {
-        console.log( JSON.stringify( values, null, 2 ) );
-        const translation = {
-          userId: 123456,
-          sim: params.sim,
-          locale: params.locale,
-          translationFormData: values
-        };
-        const [ saved, setSaved ] = useState( false );
-        const [ submitted, setSubmitted ] = useState( false );
-        useEffect( async () => {
-          if ( document.activeElement.dataset.flag === 'save' ) {
-            let wantsToSave = window.confirm( `If you have a translation saved for ${params.sim} in locale ${params.locale}, it will be overwritten.` );
-            if ( wantsToSave ) {
-              const postRes = await axios.post( '/translate/api/saveTranslation', translation );
-              console.log( postRes.data );
-              setSaved( true );
-            }
-          }
-          else if ( document.activeElement.dataset.flag === 'submit' ) {
-            let wantsToSubmit = window.confirm( `Are you sure you want to submit your translation for ${params.sim} in locale ${params.locale}?` );
-            if ( wantsToSubmit ) {
-              const postRes = await axios.post( '/translate/api/submitTranslation', translation );
-              console.log( postRes.data );
-              setSubmitted( true );
-            }
-          }
-        } );
-        if ( saved ) {
-          alert( 'Translation saved. Redirecting you to the PhET Translation Tool home page.' );
-          navigate( '/translate' );
-        }
-        else if ( submitted ) {
-          alert( 'Translation submitted. Your translation should appear on the PhET website in about half an hour. Redirecting you to the PhET Translation Tool home page.' );
-          navigate( '/translate' );
-        }
-      }}
+      onSubmit={handleSubmit}
     >
       <Form>
         <TranslationTable translationFormData={translationFormData}/>
