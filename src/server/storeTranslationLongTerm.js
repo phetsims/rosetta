bc71050aeef9836a9c48c28f8893f833913d00df
Liam Mulhall <mulhall.edward.liam@gmail.com>
2021-12-01 16:09:33 -0700

    Add Func to Prepare Translation for Long-Term...
    ...storage.

--- src/server/storeTranslationLongTerm.js
+++ src/server/storeTranslationLongTerm.js
@@ -3,7 +3,9 @@
 import logger from './logger.js';
 
 const storeTranslationLongTerm = translation => {
+  logger.info( `storing ${translation.locale}/${translation.sim} translation in long-term storage` );
   logger.info( JSON.stringify( translation, null, 2 ) );
+  logger.info( `stored ${translation.locale}/${translation.sim} translation in long-term storage` );
 };
 
 export { storeTranslationLongTerm as default };
