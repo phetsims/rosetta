ed31725dae3f166e804f8bf8fbf85bc83bc05aeb
Liam Mulhall <mulhall.edward.liam@gmail.com>
2022-01-01 15:02:14 -0700

    Optimize `getTranslationFormData`
    Pass `categorizedStringKeys` as a parameter to eliminate redundant
    calls to `getCategorizedStringKeys`.

--- src/server/api/tmp/commonTranslatedStringKeysAndStrings.js
+++ src/server/api/tmp/commonTranslatedStringKeysAndStrings.js
@@ -1,11 +1,13 @@
 // Copyright 2021, University of Colorado Boulder
 
+import getCategorizedStringKeys from '../../getCategorizedStringKeys.js';
 import getCommonTranslatedStringKeysAndStrings from '../../getCommonTranslatedStringKeysAndStrings.js';
 import logger from '../../logger.js';
 
 const commonTranslatedStringKeysAndStrings = async ( req, res ) => {
   try {
-    const commonTranslatedStringKeysAndStrings = await getCommonTranslatedStringKeysAndStrings( req.params.simOrLibName, req.params.locale );
+    const categorizedStringKeys = await getCategorizedStringKeys( req.params.simOrLibName );
+    const commonTranslatedStringKeysAndStrings = await getCommonTranslatedStringKeysAndStrings( req.params.simOrLibName, req.params.locale, categorizedStringKeys );
     logger.info( `responding with ${req.params.locale}/${req.params.simOrLibName}'s common translated string keys and strings` );
     res.json( commonTranslatedStringKeysAndStrings );
   }
