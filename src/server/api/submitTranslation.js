4cd81036a83ac2665ad0d197ae13344c71e8c02a
Liam Mulhall <mulhall.edward.liam@gmail.com>
2021-12-31 19:08:17 -0700

    Add Basic Push Trans. to GitHub
    Add basic functionality for pushing a translation to GitHub for
    long-term storage.

--- src/server/api/submitTranslation.js
+++ src/server/api/submitTranslation.js
@@ -4,12 +4,12 @@ import logger from '../logger.js';
 import prepareTranslationForLongTermStorage from '../prepareTranslationForLongTermStorage.js';
 import storeTranslationLongTerm from '../storeTranslationLongTerm.js';
 
-const submitTranslation = ( req, res ) => {
+const submitTranslation = async ( req, res ) => {
   try {
     logger.info( `sending ${req.body.locale}/${req.body.sim} translation to be prepared for long-term storage` );
     const preparedTranslation = prepareTranslationForLongTermStorage( req.body );
     logger.info( `sending ${req.body.locale}/${req.body.sim} translation to be stored long-term` );
-    storeTranslationLongTerm( preparedTranslation );
+    await storeTranslationLongTerm( preparedTranslation );
     res.send( 'translation submitted' );
   }
   catch( e ) {
--- src/server/storeTranslationLongTerm.js
+++ src/server/storeTranslationLongTerm.js
@@ -1,10 +1,42 @@
 // Copyright 2021, University of Colorado Boulder
 
+import config from './config.js';
+import { encode } from 'js-base64';
 import logger from './logger.js';
+import { Octokit } from '@octokit/rest';
 
-const storeTranslationLongTerm = translation => {
+const storeTranslationLongTerm = async translation => {
   logger.info( `storing ${translation.locale}/${translation.sim} translation in long-term storage` );
   logger.info( JSON.stringify( translation, null, 2 ) );
+  try {
+
+    // make the translation a string and use base 64 encoding
+    const encodedTranslation = encode( JSON.stringify( translation, null, 2 ) );
+
+    // push the translation to github
+    const octokit = new Octokit( {
+      auth: config.LIAM_BABEL_PAT
+    } );
+    const { data } = await octokit.repos.createOrUpdateFileContents( {
+      owner: 'liam-mulhall',
+      repo: 'babel',
+      path: 'test-translation-2.json',
+      message: 'test',
+      content: encodedTranslation,
+      committer: {
+        name: 'Liam',
+        email: 'dummy@dummy.com'
+      },
+      author: {
+        name: 'Liam',
+        email: 'dummy@dummy.com'
+      }
+    } );
+    logger.info( data );
+  }
+  catch( e ) {
+    logger.error( e );
+  }
   logger.info( `stored ${translation.locale}/${translation.sim} translation in long-term storage` );
 };
 
