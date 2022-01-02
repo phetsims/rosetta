c608f14509e401f763016660fc7204bf6ffe34bb
Liam Mulhall <mulhall.edward.liam@gmail.com>
2022-01-01 20:20:00 -0700

    Add JSDoc and Comments

--- src/client/components/LocaleSelect.js
+++ src/client/components/LocaleSelect.js
@@ -1,12 +1,32 @@
 // Copyright 2021, University of Colorado Boulder
 
+/**
+ * We define the locale select. We sort the locales by name. The name is usually the English name of the language.
+ *
+ * @author Liam Mulhall
+ */
+
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
+/**
+ * This component is a select (commonly referred to as a dropdown) for locales. The locales in this select look like
+ *
+ * English (en)
+ * German (de)
+ * Spanish (es)
+ *
+ * and so on. We have the English name of the language followed by the locale code in parentheses.
+ *
+ * @param field
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const LocaleSelect = ( { field } ) => { // eslint-disable-line react/prop-types
 
   const [ localeInfo, setLocaleInfo ] = useState( {} );
 
+  // get locale info from the backend
   useEffect( async () => {
     try {
       const localeInfoRes = await axios.get( '/translate/api/localeInfo' );
@@ -17,12 +37,15 @@ const LocaleSelect = ( { field } ) => { // eslint-disable-line react/prop-types
     }
   }, [] );
 
+  // sort the locales by name
+  // in other words, sort the languages alphabetically
   const localeNamesAndCodes = [];
   for ( const localeCode in localeInfo ) {
     localeNamesAndCodes.push( [ localeInfo[ localeCode ].name, localeCode ] );
   }
   localeNamesAndCodes.sort();
 
+  // create a list of locale options to iterate over
   const localeOptions = [];
   for ( const locale of localeNamesAndCodes ) {
     localeOptions.push(
@@ -37,6 +60,8 @@ const LocaleSelect = ( { field } ) => { // eslint-disable-line react/prop-types
 
   return (
     <select {...field}>
+
+      {/* iterate over locale options */}
       {localeOptions}
     </select>
   );
