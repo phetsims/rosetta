51fa7e42452ab51f420e8908bb06f624bc2b4e36
Liam Mulhall <mulhall.edward.liam@gmail.com>
2022-01-10 14:30:13 -0700

    docs: Add JSDoc and Comments

--- src/server/getCommonTranslatedStringKeysAndStrings.js
+++ src/server/getCommonTranslatedStringKeysAndStrings.js
@@ -1,26 +1,54 @@
 // Copyright 2021, University of Colorado Boulder
 
+/**
+ * Export a function that returns an array of arrays. Each sub-array has two elements: (1) a common translated string
+ * key, and (2) the string corresponding to (1).
+ *
+ * @author Liam Mulhall
+ */
+
 import axios from 'axios';
 import getRepoNameFromStringKeyWithRepoName from './getRepoNameFromStringKeyWithRepoName.js';
 import getStringKeyFromStringKeyWithRepoName from './getStringKeyFromStringKeyWithRepoName.js';
 import getTranslatedStringFileUrl from './getTranslatedStringFileUrl.js';
 import logger from './logger.js';
 
+/**
+ * Return a list of ordered pairs where each ordered pair is a common translated string key followed by its value
+ * (its string). This is implemented as an array of arrays where each sub-array has two elements, namely the common
+ * translated string key and its string.
+ *
+ * @param {String} simName - sim name
+ * @param {String} locale - two-letter ISO 639-1 locale code, e.g. es for Spanish
+ * @param {{simSpecific: String[], common: String[]}} categorizedStringKeys - string keys categorized into common and sim-specific
+ * @param {String[]} stringKeysWithRepoName - string keys with their respective repo names for the specified sim
+ * @returns {Promise<String[][]>}
+ */
 const getCommonTranslatedStringKeysAndStrings = async ( simName, locale, categorizedStringKeys, stringKeysWithRepoName ) => {
   logger.info( `getting ${simName}'s common translated string keys and strings` );
   const commonTranslatedStringKeysAndStrings = new Map();
   try {
-    const commonStringKeys = categorizedStringKeys.common;
+
+    // map string keys in the sim to their respective repo names
     const stringKeyToRepoName = new Map();
     for ( const stringKeyWithRepoName of stringKeysWithRepoName ) {
       const stringKey = getStringKeyFromStringKeyWithRepoName( stringKeyWithRepoName );
       const repoName = getRepoNameFromStringKeyWithRepoName( stringKeyWithRepoName );
       stringKeyToRepoName.set( stringKey, repoName );
     }
+
+    // for each common string key in the sim...
+    const commonStringKeys = categorizedStringKeys.common;
     for ( const stringKey of commonStringKeys ) {
+
+      // if the string key in the map we made earlier...
       if ( stringKeyToRepoName.has( stringKey ) ) {
+
+        // get the string key's repo url
         const repoName = stringKeyToRepoName.get( stringKey );                          // inefficient
         const translatedStringFileUrl = getTranslatedStringFileUrl( repoName, locale ); // inefficient
+
+        // go into the string file for that repo and grab the string key's value
         try {
           const translatedStringKeysAndStringsRes = await axios.get( translatedStringFileUrl );
           const translatedStringKeysAndStrings = translatedStringKeysAndStringsRes.data;
@@ -47,6 +75,8 @@ const getCommonTranslatedStringKeysAndStrings = async ( simName, locale, categor
     logger.error( e );
   }
   logger.info( `got ${simName}'s common translated string keys and strings; returning them` );
+
+  // use spread operator and brackets to return an array
   return [ ...commonTranslatedStringKeysAndStrings ];
 };
 
