43e96fcb5d999c264e5683a7b160f36fd79c4300
Liam Mulhall <liam.mulhall@colorado.edu>
2022-02-16 14:22:57 -0700

    feat: Finish Work on `getKeysValuesAndRepos`

--- src/server/getTranslatedStringFileUrl.js
+++ src/server/getTranslatedStringFileUrl.js
@@ -13,14 +13,14 @@ import logger from './logger.js';
 /**
  * Return the URL for a translated sim's string file.
  *
- * @param {String} simName - sim name
+ * @param {String} simOrLibName - sim name or library name
  * @param {String} locale - two-letter ISO 639-1 locale code, e.g. es for Spanish
  * @returns {String} - translated sim's string file URL
  */
-const getTranslatedStringFile = ( simName, locale ) => {
-  logger.verbose( `getting ${simName}/${locale}'s translated string file url` );
-  logger.verbose( `got ${simName}/${locale}'s translated string file url; returning it` );
-  return `${config.GITHUB_URL}/babel/${config.BABEL_BRANCH}/${simName}/${simName}-strings_${locale}.json`;
+const getTranslatedStringFile = ( simOrLibName, locale ) => {
+  logger.verbose( `getting ${simOrLibName}/${locale}'s translated string file url` );
+  logger.verbose( `got ${simOrLibName}/${locale}'s translated string file url; returning it` );
+  return `${config.GITHUB_URL}/babel/${config.BABEL_BRANCH}/${simOrLibName}/${simOrLibName}-strings_${locale}.json`;
 };
 
 export default getTranslatedStringFile;
