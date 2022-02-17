9da175b5ba76f65c58d32e5067beef4441e5b9c9
Liam Mulhall <liam.mulhall@colorado.edu>
2022-02-16 17:13:40 -0700

    feat: Replace `oldGetTranslationFormData`

--- src/server/api/translationFormData.js
+++ src/server/api/translationFormData.js
@@ -6,6 +6,11 @@
  * @author Liam Mulhall
  */
 
+import getCategorizedStringKeys from '../getCategorizedStringKeys.js';
+import getSimHtml from '../getSimHtml.js';
+import getSimNames from '../getSimNames.js';
+import getSimUrl from '../getSimUrl.js';
+import getStringKeysWithRepoName from '../getStringKeysWithRepoName.js';
 import getTranslationFormData from '../getTranslationFormData.js';
 import logger from '../logger.js';
 
@@ -18,7 +23,18 @@ import logger from '../logger.js';
  */
 const translationFormData = async ( req, res ) => {
   try {
-    const translationFormData = await getTranslationFormData( req.params.simName, req.params.locale );
+    const simNames = await getSimNames();
+    const simUrl = getSimUrl( req.params.simName );
+    const simHtml = await getSimHtml( simUrl );
+    const stringKeysWithRepoName = getStringKeysWithRepoName( simHtml );
+    const categorizedStringKeys = await getCategorizedStringKeys( req.params.simName, simNames, stringKeysWithRepoName );
+    const translationFormData = await getTranslationFormData(
+      req.params.simName,
+      req.params.locale,
+      simNames,
+      stringKeysWithRepoName,
+      categorizedStringKeys
+    );
     logger.info( `responding with ${req.params.locale}/${req.params.simName}'s translation form data` );
     res.header( 'Content-Type', 'application/json' );
     res.send( JSON.stringify( translationFormData, null, 2 ) );
--- test/translationFormData.test.js
+++ test/translationFormData.test.js
@@ -1,8 +1,12 @@
 // Copyright 2021, University of Colorado Boulder
 
 import config from '../src/server/config.js';
+import getCategorizedStringKeys from '../src/server/getCategorizedStringKeys.js';
+import getSimHtml from '../src/server/getSimHtml.js';
+import getSimNames from '../src/server/getSimNames.js';
+import getSimUrl from '../src/server/getSimUrl.js';
+import getStringKeysWithRepoName from '../src/server/getStringKeysWithRepoName.js';
 
-// import getSimNames from '../src/server/getSimNames.js';
 import getTranslationFormData from '../src/server/getTranslationFormData.js';
 import logger from '../src/server/logger.js';
 import getRandomLocales from './utils/getRandomLocales.js';
@@ -13,11 +17,30 @@ QUnit.module( 'translationFormData' );
 QUnit.test( 'translationFormData should return truthy', async assert => {
 
   if ( config.ENVIRONMENT === 'development' ) {
+
     const numberOfSimsToTest = 1;
+
     const randomSimNames = await getRandomSimNames( numberOfSimsToTest );
     const randomLocales = await getRandomLocales( numberOfSimsToTest );
+
+    const simNames = await getSimNames();
+
     for ( let i = 0; i < numberOfSimsToTest; i++ ) {
-      const translationFormData = await getTranslationFormData( randomSimNames[ i ], randomLocales[ i ] );
+      const simUrl = getSimUrl( randomSimNames[ i ] );
+      const simHtml = await getSimHtml( simUrl );
+      const stringKeysWithRepoName = getStringKeysWithRepoName( simHtml );
+      const categorizedStringKeys = await getCategorizedStringKeys(
+        randomSimNames[ i ],
+        simNames,
+        stringKeysWithRepoName
+      );
+      const translationFormData = await getTranslationFormData(
+        randomSimNames[ i ],
+        randomLocales[ i ],
+        simNames,
+        stringKeysWithRepoName,
+        categorizedStringKeys
+      );
       assert.ok( translationFormData );
     }
   }
