089bfcbf64b0c0fac5916c535acb7fbde69aa7f2
Liam Mulhall <liam.mulhall@colorado.edu>
2022-03-03 14:19:14 -0700

    feat: Add Work on `storeTranslationLongTerm.js`

--- src/server/storeTranslationLongTerm.js
+++ src/server/storeTranslationLongTerm.js
@@ -6,20 +6,55 @@
  * @author Liam Mulhall
  */
 
-// import config from './config.js';
-// import { encode } from 'js-base64';
-// import { Octokit } from '@octokit/rest';
-
+import config from './config.js';
 import logger from './logger.js';
+import { Octokit } from '@octokit/rest';
+import { encode } from 'js-base64';
 
 /**
  * Save the translation to long-term storage.
  *
- * @param translation - translation received from client
+ * @param {Object} preparedTranslation - exact translation file contents for each repo in a translation
  */
-const storeTranslationLongTerm = async translation => {
+const storeTranslationLongTerm = async preparedTranslation => {
 
-  logger.info( `storing translation of ${translation.locale}/${translation.simName} long-term` );
+  logger.info( `storing translation of ${preparedTranslation.locale}/${preparedTranslation.simName} long-term` );
+
+  // iterate through each repo in the prepared translation and save its translation file contents to long-term storage
+  const contents = preparedTranslation.translationFileContents;
+  for ( const repo of Object.keys( contents ) ) {
+    try {
+
+      // make the translation file contents a string and use base 64 encoding
+      const encodedTranslationFileContents = encode( JSON.stringify( contents[ repo ], null, 2 ) );
+
+      // create an interface to long-term storage
+      const octokit = new Octokit( {
+        auth: config.LIAM_BABEL_PAT
+      } );
+
+      // save translation file contents to long-term storage
+      const { data } = await octokit.repos.createOrUpdateFileContents( {
+        owner: 'liam-mulhall',
+        repo: 'babel',
+        path: `test-${preparedTranslation.simName}-${preparedTranslation.locale}-${repo}-translation.json`,
+        message: 'test',
+        content: encodedTranslationFileContents,
+        committer: {
+          name: 'Liam',
+          email: 'dummy@dummy.com'
+        },
+        author: {
+          name: 'Liam',
+          email: 'dummy@dummy.com'
+        }
+      } );
+      logger.error( data );
+    }
+    catch( e ) {
+      logger.error( e );
+    }
+  }
 
   // todo: finish this when you're done preparing the translation for long-term storage
   // logger.info( `storing ${translation.locale}/${translation.sim} translation in long-term storage` );
@@ -55,7 +90,7 @@ const storeTranslationLongTerm = async translation => {
   // }
   // logger.info( `stored ${translation.locale}/${translation.sim} translation in long-term storage` );
 
-  logger.info( `stored translation of ${translation.locale}/${translation.simName} long-term` );
+  logger.info( `stored translation of ${preparedTranslation.locale}/${preparedTranslation.simName} long-term` );
 };
 
 export default storeTranslationLongTerm;
