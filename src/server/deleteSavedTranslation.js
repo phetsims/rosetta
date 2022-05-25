8df02f5ab9edbc2dc92ffe0973f076a4fc451583
Liam Mulhall <liam.mulhall@colorado.edu>
2022-05-25 15:04:08 -0600

    feat: Improve Storage of Strings
    * Export connection to short-term storage collection.
    * Add module for deleting saved translations.
    * Use phetsims/babel for long-term storage.

---
+++ src/server/deleteSavedTranslation.js
@@ -0,0 +1,33 @@
+// Copyright 2021-2022, University of Colorado Boulder
+
+/**
+ * Export a function that deletes saved translation(s) (although technically there should only ever be one) that match
+ * the provided
+ *
+ * @author Liam Mulhall
+ */
+
+import logger from './logger.js';
+import { shortTermStringStorageCollection } from './getShortTermStringStorageCollection.js';
+
+/**
+ * Delete any translations that match the provided user ID, sim name, and locale.
+ *
+ * @param {Object} idSimNameAndLocale - an object containing the user ID, sim name, and locale for a translation
+ */
+const deleteSavedTranslation = async idSimNameAndLocale => {
+  logger.info( `attempting to delete old ${idSimNameAndLocale.locale}/${idSimNameAndLocale.simName} translation(s) in short-term storage` );
+  try {
+    const oldSavedTranslation = await shortTermStringStorageCollection.find( idSimNameAndLocale );
+    if ( oldSavedTranslation ) {
+      logger.info( `old saved ${idSimNameAndLocale.locale}/${idSimNameAndLocale.simName} translation(s) with same user id extant; deleting them` );
+      await shortTermStringStorageCollection.deleteMany( idSimNameAndLocale );
+    }
+  }
+  catch( e ) {
+    logger.error( e );
+  }
+  logger.info( `done attempting to delete ${idSimNameAndLocale.locale}/${idSimNameAndLocale.simName} translation(s) in short-term storage` );
+};
+
+export default deleteSavedTranslation;
