55d0d0071a5f543f6e49c72f5132cd97a0a8a5d8
Liam Mulhall <liam.mulhall@colorado.edu>
2022-06-08 16:59:57 -0600

    feat: [WIP] Store Transl. Using Octonode

--- src/server/storeTranslationLongTerm.js
+++ src/server/storeTranslationLongTerm.js
@@ -6,13 +6,12 @@
  * @author Liam Mulhall
  */
 
-import axios from 'axios';
+import github from 'octonode';
 import config from './config.js';
-import deleteSavedTranslation from './deleteSavedTranslation.js';
-import getBlobSha from './getBlobSha.js';
 import logger from './logger.js';
-import { Octokit } from '@octokit/rest';
-import { encode } from 'js-base64';
+
+const githubInterface = github.client( config.GITHUB_PAT );
+const longTermStorage = githubInterface.repo( 'phetsims/babel' );
 
 /**
  * Save the translation to long-term storage.
@@ -20,99 +19,49 @@ import { encode } from 'js-base64';
  * @param {Object} preparedTranslation - exact translation file contents for each repo in a translation
  */
 const storeTranslationLongTerm = async preparedTranslation => {
+  if ( config.PERFORM_STRING_COMMITS === 'true' ) {
 
-  if ( config.PERFORM_STRING_COMMITS ) {
-
-    // create an interface to long-term storage
-    const octokit = new Octokit( {
-      auth: config.GITHUB_PAT
-    } );
-
-    logger.info( `storing translation of ${preparedTranslation.locale}/${preparedTranslation.simName} long-term` );
-
-    // iterate through each repo in the prepared translation and save its translation file contents to long-term storage
     const contents = preparedTranslation.translationFileContents;
     for ( const repo of Object.keys( contents ) ) {
 
       // check to see if the object is not empty (i.e. strings were translated in repo)
       if ( Object.keys( contents[ repo ] ).length !== 0 ) {
+
+        logger.info( `storing translation of strings in ${repo} long-term` );
+
+        // make the translation file contents a string and use base 64 encoding
+        const fileContents = JSON.stringify( contents[ repo ], null, 2 );
+
+        const translationFilePath = `${repo}/${repo}-strings_${preparedTranslation.locale}.json`;
+
         try {
 
-          logger.info( `storing translation of strings in ${repo} long-term` );
-
-          // make the translation file contents a string and use base 64 encoding
-          const encodedTranslationFileContents = encode( JSON.stringify( contents[ repo ], null, 2 ) );
-
-          const translationFilePath = `${repo}/${repo}-strings_${preparedTranslation.locale}.json`;
-
-          // if a translation file exists for the repo/locale we're dealing with, get its sha
-          let translationFileSha = null;
-          try {
-            const rawGithubFileUrl = `${config.GITHUB_URL}/babel/${config.BABEL_BRANCH}/${translationFilePath}`;
-            logger.info( `trying to get ${rawGithubFileUrl}` );
-            const rawGithubFileRes = await axios.get( rawGithubFileUrl );
-            if ( rawGithubFileRes.status === 200 ) {
-
-              // the file exists in babel, so let's get its sha
-              translationFileSha = await getBlobSha( translationFilePath );
-            }
-          }
-          catch( e ) {
-            if ( e.status === 404 ) {
-              logger.info( `no translation file exists for ${repo}/${preparedTranslation.locale}` );
-            }
-            logger.error( e );
-          }
-
-          // todo: change params when done
-          // save translation file contents to long-term storage
-          const params = {
-            owner: 'phetsims',
-            repo: 'babel',
-            branch: config.BABEL_BRANCH,
-            path: translationFilePath,
-            message: `automated commit from rosetta for sim/lib ${repo}, locale ${preparedTranslation.locale}`,
-            content: encodedTranslationFileContents,
-            committer: {
-              name: config.COMMITTER_NAME,
-              email: config.COMMITTER_EMAIL
-            },
-            author: {
-              name: config.COMMITTER_NAME,
-              email: config.COMMITTER_EMAIL
-            }
-          };
-          if ( translationFileSha ) {
-            params.sha = translationFileSha;
-          }
-          await octokit.repos.createOrUpdateFileContents( params );
-
-          logger.info( `stored translation of strings in ${repo} long-term` );
-
-          await deleteSavedTranslation( {
-            userId: preparedTranslation.userId,
-            simName: preparedTranslation.simName,
-            locale: preparedTranslation.locale
-          } );
+          // try to update the file
+          const contentsRes = await longTermStorage.contentsAsync( translationFilePath, config.BABEL_BRANCH );
+          const commitMessage = `automated commit from rosetta for sim/lib ${repo}, locale ${preparedTranslation.locale}`;
+          await longTermStorage.updateContentsAsync(
+            translationFilePath,
+            commitMessage,
+            fileContents,
+            contentsRes[ 0 ].sha,
+            config.BABEL_BRANCH
+          );
         }
         catch( e ) {
+          try {
+
+            // try to create the file
+            console.log( 'TRY TO CREATE FILE' );
+          }
+          catch( e ) {
+            logger.error( e );
+          }
           logger.error( e );
         }
       }
-
-      // strings were not translated in repo
-      else {
-        logger.info( `no strings translated in ${repo}; not storing long-term` );
-      }
     }
 
-    logger.info( `stored translation of ${preparedTranslation.locale}/${preparedTranslation.simName} long-term` );
   }
-  else {
-    logger.info( 'rosetta not configured to perform string commits; check your config file' );
-    logger.info( `not storing translation of ${preparedTranslation.locale}/${preparedTranslation.simName} long-term` );
-  }
-
 };
 
 export default storeTranslationLongTerm;
