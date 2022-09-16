fe5b70469f31c05498b2c7d7ba936235fc00fc56
Liam Mulhall <liammulh@gmail.com>
2022-09-16 10:52:12 -0600

    feat: add helper function

---
+++ src/server/translationApi/triggerBuild/isValidUserId.js
@@ -0,0 +1,35 @@
+// Copyright, 2022 University of Colorado Boulder
+
+import axios from 'axios';
+import config from '../../../common/config.js';
+import logger from '../logger.js';
+
+const isValidUserId = async ( simName, locale, userId ) => {
+
+  let ret = false;
+
+  const stringFileUrl = config.GITHUB_URL +
+                        `phetsims/babel/master/${simName}/${simName}-strings_${locale}.json`;
+
+  // Try to verify whether the supplied user ID exists in the string file for the given sim/locale.
+  try {
+    const stringFileRes = await axios.get( stringFileUrl );
+    const stringFile = stringFileRes.data;
+    const setOfUserIds = new Set();
+    for ( const key of Object.keys( stringFile ) ) {
+      const historyArray = stringFile[ key ].history;
+      for ( const historyEntry of historyArray ) {
+        setOfUserIds.add( historyEntry.userId );
+      }
+    }
+    if ( setOfUserIds.has( userId ) ) {
+      ret = true;
+    }
+  }
+  catch( e ) {
+    logger.error( e );
+  }
+  return ret;
+};
+
+export default isValidUserId;
