11b6c8fde7701a42625790249aec280440e1bc32
Liam Mulhall <liammulh@gmail.com>
2022-09-15 18:44:36 -0600

    feat: add route

---
+++ src/server/translationApi/api/triggerBuild.js
@@ -0,0 +1,21 @@
+// Copyright 2022, University of Colorado Boulder
+
+import logger from '../logger.js';
+
+/**
+ * API function. Respond with the current SHA of the running instance of Rosetta.
+ *
+ * @param {Object} req - the Express request object
+ * @param {Object} res - the Express response object
+ */
+const triggerBuild = ( req, res ) => {
+  logger.info( 'trigger build' );
+  const buildObject = {
+    sim: req.params.simName,
+    locale: req.params.locale,
+    userId: req.params.userId
+  };
+  res.json( buildObject );
+};
+
+export default triggerBuild;
