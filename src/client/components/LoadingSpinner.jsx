233568aaec9630805ee9c8175e697b0406f9de5d
Liam Mulhall <liammulh@gmail.com>
2022-09-18 14:07:12 -0600

    feat: put spinner in its own component

---
+++ src/client/components/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+// Copyright 2022, University of Colorado Boulder
+
+const LoadingSpinner = () => {
+  return (
+    <div className='spinner-border text-primary' role='status'>
+      <span className='visually-hidden'>Loading...</span>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
