6e69d0f056de97e0cade9a692b723e389803dc0f
Liam Mulhall <liammulh@gmail.com>
2022-09-30 15:00:47 -0600

    checkpoint

--- src/client/components/TranslationRow.jsx
+++ src/client/components/TranslationRow.jsx
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import { useField } from 'formik';
+import InputErrorMessage from './InputErrorMessage.jsx';
 
 /**
  * This component is a row in the translation table. It has the string key, the English string, and an input for
@@ -19,13 +20,19 @@ const TranslationRow = props => {
   // get field props for the input
   const [ field ] = useField( props );
 
+  const change = () => {
+    console.log( props.errors );
+    console.log( props.touched );
+  };
+
   return (
     <tr>
       <td>{props.stringKey}</td>
       <td>{props.englishString}</td>
 
       {/* use the spread operator to give the input each of the props in the field object */}
-      <td><input {...field}/></td>
+      <td><input onChange={change} {...field}/></td>
+      <InputErrorMessage errors={props.errors} touched={props.touched} fieldKey={props.key}/>
     </tr>
   );
 };
