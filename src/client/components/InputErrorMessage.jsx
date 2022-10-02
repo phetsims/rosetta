b78b96ac472e4b6dd1c05cc1c44b4da34586bb59
Liam Mulhall <liammulh@gmail.com>
2022-10-02 10:08:05 -0600

    add TODO comments

--- src/client/components/InputErrorMessage.jsx
+++ src/client/components/InputErrorMessage.jsx
@@ -12,6 +12,11 @@ const InputErrorMessage = ( { errors, touched, fieldKey } ) => {
       pH: { translated: true }
   */
   // TODO: Change conditional to reflect true shape of errors and touched objects.
+  // TODO: It seems like the state for the errors object isn't reaching this component?
+  // TODO: Because of https://stackoverflow.com/questions/41233458/react-child-component-not-updating-after-parent-state-change?
+  // TODO: Would something like this https://stackoverflow.com/a/41679634 help?
+  // TODO: Using something like {...props} on parent components?
+  // TODO: And https://formik.org/docs/api/useField?
   if ( errors[ fieldKey ] && touched[ fieldKey ] ) {
     jsx = <div style={divStyle}>{errors[ fieldKey ]}</div>;
   }
