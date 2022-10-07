2d507d4e16aac1756e662bd3280b19be39ddb709
Liam Mulhall <liammulh@gmail.com>
2022-10-07 14:00:01 -0600

    try matching brace expr

--- src/client/utils/makeValidationSchema.js
+++ src/client/utils/makeValidationSchema.js
@@ -2,20 +2,27 @@
 
 import * as Yup from 'yup';
 
+const SINGLE_BRACE_REGEX = /\{\d+\}/g;
+
+//const DOUBLE_BRACE_REGEX = /\{\{\w+\}\}/g;
+
 const makeValidationSchema = translationFormData => {
   let validationSchema;
   try {
     const simSpecificKeys = Object.keys( translationFormData.simSpecific ).map(
-      key => key.split( '_DOT_' ).join( '.' )
+      key => key.split( '.' ).join( '_DOT_' )
     );
     const simSpecificValidationObject = {};
     for ( const key of simSpecificKeys ) {
-      simSpecificValidationObject[ key ] = Yup.object( {
-        translated: Yup.string().required()
-      } );
+      const englishValue = translationFormData.simSpecific[ key ].english;
+      if ( SINGLE_BRACE_REGEX.test( englishValue ) ) {
+        simSpecificValidationObject[ key ] = Yup.object( {
+          translated: Yup.string().matches( SINGLE_BRACE_REGEX, 'Must have braces' )
+        } );
+      }
     }
     const commonKeys = Object.keys( translationFormData.common ).map(
-      key => key.split( '_DOT_' ).join( '.' )
+      key => key.split( '.' ).join( '_DOT_' )
     );
     const commonValidationObject = {};
     for ( const key of commonKeys ) {
