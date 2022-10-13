97277536110c861993d0643e49d07169f1825222
Liam Mulhall <liammulh@gmail.com>
2022-10-13 15:15:36 -0600

    feat: add file for validating braces

---
+++ src/client/utils/isValidBracePattern.js
@@ -0,0 +1,12 @@
+// Copyright 2022, University of Colorado Boulder
+
+// const SINGLE_BRACE_PATTERN_REGULAR_EXP = /\{\d+\}/g;
+// const DOUBLE_BRACE_PATTERN_REGULAR_EXP = /\{\{\w+\}\}/g;
+
+const isValidBracePattern = ( translatedValue, englishValue ) => {
+  console.log( `englishValue = ${englishValue}` );
+  console.log( `translatedValue = ${translatedValue}` );
+  return false;
+};
+
+export default isValidBracePattern;
