688b183ec2e8c1a8d3ea9c32d04af829655452d1
Liam Mulhall <liammulh@gmail.com>
2022-10-20 14:47:19 -0600

    feat: add work on form validation

--- src/client/utils/isValidBracePattern.js
+++ src/client/utils/isValidBracePattern.js
@@ -1,27 +1,34 @@
 // Copyright 2022, University of Colorado Boulder
 
-// import clientConstants from './clientConstants.js';
-
-// const globalSingleBraceRegex = new RegExp( clientConstants.singleBraceRegex, 'g' );
-// const globalDoubleBraceRegex = new RegExp( clientConstants.doubleBraceRegex, 'g' );
+import clientConstants from './clientConstants.js';
 
 const isValidBracePattern = ( translatedValue, englishValue ) => {
+  if ( translatedValue === undefined ) {
 
-  // let isValid = false;
-  // if ( translatedValue === undefined ) {
-  //
-  //   // If the user hasn't supplied a translation yet, the value will be undefined.
-  //   // We want blank translations to be valid.
-  //   isValid = true;
-  // }
-  // const singleBracePlaceHolders = englishValue.match( clientConstants.singleBraceRegex ) || [];
-  // const doubleBracePlaceHolders = englishValue.match( clientConstants.doubleBraceRegex ) || [];
-  // console.log( `englishValue = ${englishValue}` );
-  // console.log( `translatedValue = ${translatedValue}` );
-  // console.log( `single braces = ${Array.isArray( singleBracePlaceHolders )}` );
-  // console.log( `double braces = ${Array.isArray( doubleBracePlaceHolders )}` );
+    // If the user hasn't supplied a translation yet, the value will be undefined.
+    // We want blank translations to be valid. We can bail at this point.
+    return true;
+  }
+  let isValid = false;
+  const englishSingleBraces = englishValue.match( clientConstants.singleBraceRegex ) || [];
+  const englishDoubleBraces = englishValue.match( clientConstants.doubleBraceRegex ) || [];
+  const translatedSingleBraces = translatedValue.match( clientConstants.singleBraceRegex ) || [];
+  const translatedDoubleBraces = translatedValue.match( clientConstants.doubleBraceRegex ) || [];
 
-  return true;
+  // Set up booleans.
+  const hasSingleBraces = englishSingleBraces.length > 0;
+  const hasDoubleBraces = englishDoubleBraces.length > 0;
+  const numSingleBracesIsSame = englishSingleBraces.length === translatedSingleBraces.length;
+  const numDoubleBracesIsSame = englishDoubleBraces.length === translatedDoubleBraces.length;
+
+  if ( hasSingleBraces && numSingleBracesIsSame ) {
+    isValid = true;
+  }
+  else if ( hasDoubleBraces && numDoubleBracesIsSame ) {
+    isValid = true;
+  }
+
+  return isValid;
 };
 
 export default isValidBracePattern;
