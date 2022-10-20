d829ccf70eba763375ff325240e88455ee93bd4c
Liam Mulhall <liammulh@gmail.com>
2022-10-20 16:59:09 -0600

    feat: add work for form validation

--- src/client/utils/isValidBracePattern.js
+++ src/client/utils/isValidBracePattern.js
@@ -19,17 +19,32 @@ const isValidBracePattern = ( translatedValue, englishValue ) => {
   const translatedSingleBraces = translatedValue.match( clientConstants.singleBraceRegex ) || [];
   const translatedDoubleBraces = translatedValue.match( clientConstants.doubleBraceRegex ) || [];
 
-  // Set up booleans.
   const hasSingleBraces = englishSingleBraces.length > 0;
   const hasDoubleBraces = englishDoubleBraces.length > 0;
+
+  // Generally, there should only be one type of braces, so both of these should be true.
+  // One of them is working with arrays of length zero.
   const numSingleBracesIsSame = englishSingleBraces.length === translatedSingleBraces.length;
   const numDoubleBracesIsSame = englishDoubleBraces.length === translatedDoubleBraces.length;
 
-  if ( hasSingleBraces && numSingleBracesIsSame ) {
-    isValid = true;
-  }
-  else if ( hasDoubleBraces && numDoubleBracesIsSame ) {
-    isValid = true;
+  if ( numSingleBracesIsSame && numDoubleBracesIsSame ) {
+    let numMatches = 0;
+    const braceExprList = hasSingleBraces ? englishSingleBraces :
+                          hasDoubleBraces ? englishDoubleBraces :
+                          null;
+    if ( braceExprList ) {
+      for ( const braceExpr of braceExprList ) {
+        if ( translatedSingleBraces.includes( braceExpr ) ) {
+          numMatches++;
+        }
+        else if ( translatedDoubleBraces.includes( braceExpr ) ) {
+          numMatches++;
+        }
+      }
+      if ( numMatches === braceExprList.length ) {
+        isValid = true;
+      }
+    }
   }
 
   return isValid;
