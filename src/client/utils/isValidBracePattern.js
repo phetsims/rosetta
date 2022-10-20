6906ea515f23c7173282551f0d19a2521540526f
Liam Mulhall <liammulh@gmail.com>
2022-10-19 19:10:00 -0600

    feat: add WIP for replace string object function

--- src/client/utils/isValidBracePattern.js
+++ src/client/utils/isValidBracePattern.js
@@ -1,25 +1,27 @@
 // Copyright 2022, University of Colorado Boulder
 
-import clientConstants from './clientConstants.js';
+// import clientConstants from './clientConstants.js';
 
 // const globalSingleBraceRegex = new RegExp( clientConstants.singleBraceRegex, 'g' );
 // const globalDoubleBraceRegex = new RegExp( clientConstants.doubleBraceRegex, 'g' );
 
 const isValidBracePattern = ( translatedValue, englishValue ) => {
-  let isValid = false;
-  if ( translatedValue === undefined ) {
 
-    // If the user hasn't supplied a translation yet, the value will be undefined.
-    // We want blank translations to be valid.
-    isValid = true;
-  }
-  const singleBracePlaceHolders = englishValue.match( clientConstants.singleBraceRegex ) || [];
-  const doubleBracePlaceHolders = englishValue.match( clientConstants.doubleBraceRegex ) || [];
-  console.log( `englishValue = ${englishValue}` );
-  console.log( `translatedValue = ${translatedValue}` );
-  console.log( `single braces = ${Array.isArray( singleBracePlaceHolders )}` );
-  console.log( `double braces = ${Array.isArray( doubleBracePlaceHolders )}` );
-  return isValid;
+  // let isValid = false;
+  // if ( translatedValue === undefined ) {
+  //
+  //   // If the user hasn't supplied a translation yet, the value will be undefined.
+  //   // We want blank translations to be valid.
+  //   isValid = true;
+  // }
+  // const singleBracePlaceHolders = englishValue.match( clientConstants.singleBraceRegex ) || [];
+  // const doubleBracePlaceHolders = englishValue.match( clientConstants.doubleBraceRegex ) || [];
+  // console.log( `englishValue = ${englishValue}` );
+  // console.log( `translatedValue = ${translatedValue}` );
+  // console.log( `single braces = ${Array.isArray( singleBracePlaceHolders )}` );
+  // console.log( `double braces = ${Array.isArray( doubleBracePlaceHolders )}` );
+
+  return true;
 };
 
 export default isValidBracePattern;
