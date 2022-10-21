a3bc007cd3a8fd48f8642f3d3c926e7f7f5faf2d
Liam Mulhall <liammulh@gmail.com>
2022-10-21 12:57:54 -0600

    docs/feat: add shell script

---
+++ bin/print-files-without-@author.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# We assume that all JSDoc summaries include the @author tag.
+# By searching for files that don't contain the @author tag,
+# we find the files that don't have a JSDoc summary.
+
+output_files() {
+  grep \
+  --recursive \
+  --include="*.js" \
+  --include="*.jsx" \
+  --include=".ts" \
+  --include=".tsx" \
+  --exclude-dir "node_modules" \
+  --exclude-dir "scripts" \
+  --exclude-dir "dist" \
+  --files-without-match \
+  "@author"
+}
+
+echo "Files without @author:"
+output_files
+echo "Number of files without @author:"
+output_files | wc -l
