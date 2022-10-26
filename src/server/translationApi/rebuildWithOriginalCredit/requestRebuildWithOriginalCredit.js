c36aeea69e3f0969e979d328590a9b04b7c776a1
Liam Mulhall <liammulh@gmail.com>
2022-10-26 15:18:45 -0600

    feat: finish adding trigger build

--- src/server/translationApi/rebuildWithOriginalCredit/requestRebuildWithOriginalCredit.js
+++ src/server/translationApi/rebuildWithOriginalCredit/requestRebuildWithOriginalCredit.js
@@ -1,6 +1,29 @@
 // Copyright 2022, University of Colorado Boulder
 
-const requestRebuildWithOriginalCredit = () => {
+import axios from 'axios';
+import config from '../../../common/config.js';
+import logger from '../logger.js';
+import requestBuild from '../requestBuild.js';
+import isValidLocale from './isValidLocale.js';
+import isValidSimName from './isValidSimName.js';
+import isValidUserId from './isValidUserId.js';
+
+const requestRebuildWithOriginalCredit = async ( simName, locale, userId ) => {
+  try {
+    const userDataRes = await axios.get( `${config.SERVER_URL}/services/check-login` );
+    const userData = userDataRes.data;
+    if ( userData.loggedIn && userData.teamMember ) {
+      const simNameIsValid = isValidSimName( simName );
+      const localeIsValid = isValidLocale( locale );
+      const userIdIsValid = isValidUserId( userId );
+      if ( simNameIsValid && localeIsValid && userIdIsValid ) {
+        await requestBuild( simName, locale, userId );
+      }
+    }
+  }
+  catch( e ) {
+    logger.error( e );
+  }
 };
 
 export default requestRebuildWithOriginalCredit;
