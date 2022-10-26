4c0f57678ae04d37cb1bdcf95a8edc9983ee5db6
Liam Mulhall <liammulh@gmail.com>
2022-10-26 15:18:45 -0600

    feat: finish adding trigger build

--- src/server/translationApi/api/rebuildWithOriginalCredit.js
+++ src/server/translationApi/api/rebuildWithOriginalCredit.js
@@ -1,6 +1,7 @@
 // Copyright 2022, University of Colorado Boulder
 
 import logger from '../logger.js';
+import requestRebuildWithOriginalCredit from '../rebuildWithOriginalCredit/requestRebuildWithOriginalCredit.js';
 
 /**
  * API function. Respond with the current SHA of the running instance of Rosetta.
@@ -8,16 +9,24 @@ import logger from '../logger.js';
  * @param {Object} req - the Express request object
  * @param {Object} res - the Express response object
  */
-const rebuildWithOriginalCredit = ( req, res ) => {
-  logger.info( 'trigger build' );
-  const buildObject = {
-    sim: req.params.simName,
-    locale: req.params.locale,
-    userId: req.params.userId
-  };
-  logger.info( 'build object:' );
-  logger.info( JSON.stringify( buildObject, null, 2 ) );
-  res.end();
+const rebuildWithOriginalCredit = async ( req, res ) => {
+  logger.info( 'rebuilding with original credit' );
+  try {
+    const rebuildRes = await requestRebuildWithOriginalCredit(
+      req.params.simName,
+      req.params.locale,
+      req.params.userId
+    );
+    if ( rebuildRes.status >= 200 && rebuildRes.status < 300 ) {
+      logger.info( 'rebuilt with original credit' );
+      res.end();
+    }
+  }
+  catch( e ) {
+    logger.error( 'rebuild with original credit failed' );
+    logger.error( e );
+    res.end();
+  }
 };
 
 export default rebuildWithOriginalCredit;
