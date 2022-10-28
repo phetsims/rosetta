e56d9e8d4d150a9dc1539160052efb51f1e46236
Liam Mulhall <liammulh@gmail.com>
2022-10-28 15:22:02 -0600

    feat: add locale name to translation form

--- src/client/components/TranslationFormHeader.jsx
+++ src/client/components/TranslationFormHeader.jsx
@@ -2,11 +2,21 @@
 
 /* eslint-disable react/prop-types */
 
+import { useContext } from 'react';
+import { LocaleInfoContext } from './RosettaRoutes.jsx';
+
 const TranslationFormHeader = props => {
+
+  const localeInfo = useContext( LocaleInfoContext );
+  let localeName = 'Loading...';
+  if ( Object.keys( localeInfo ).length > 0 ) {
+    localeName = localeInfo[ props.locale ].name;
+  }
+
   return (
     <div>
       <h1>Translation Form</h1>
-      <h2 className='text-muted'>Locale: {props.locale}</h2>
+      <h2 className='text-muted'>Locale: {localeName} ({props.locale})</h2>
       <h2 className='text-muted'>Sim: {props.simName}</h2>
     </div>
   );
