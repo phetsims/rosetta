d335df099e1d4d37a907be2e69444153f45637a9
Liam Mulhall <liammulh@gmail.com>
2022-10-28 21:36:43 -0600

    docs: add JSDoc

--- src/server/translationApi/getSimNamesAndTitles.js
+++ src/server/translationApi/getSimNamesAndTitles.js
@@ -1,8 +1,21 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * This file exports a function that gets an object containing string lowercase kebab case sim names
+ * (e.g. acid-base-solutions) mapped to sim titles (e.g. Acid-Base Solutions).
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import config from '../../common/config.js';
 import logger from './logger.js';
 
+/**
+ * If the sim should be visible, extract it from the simulations object provided, otherwise return false.
+ *
+ * @param {object} sim - simulations object from sim metadata
+ * @returns {boolean|string} - false if sim shouldn't be visible, sim name otherwise
+ */
 const extractSimName = sim => {
   let simName = false;
   const wantSimVisible = config.ENVIRONMENT === 'development' ||
@@ -14,6 +27,13 @@ const extractSimName = sim => {
   return simName;
 };
 
+/**
+ * Return an object containing string lowercase kebab case sim names (e.g. acid-base-solutions) mapped to sim
+ * titles (e.g. Acid-Base Solutions).
+ *
+ * @param {object} simMetadata - sim metadata obtained from the PhET website
+ * @returns {object} - sim names mapped to sim titles
+ */
 const getSimNamesAndTitles = simMetadata => {
   logger.info( 'getting sim names and titles' );
   const simNamesAndTitles = {};
