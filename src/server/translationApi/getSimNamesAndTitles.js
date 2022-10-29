e1effdf3ca5380b004504bfe826db21e0eeeba88
Liam Mulhall <liammulh@gmail.com>
2022-10-28 21:39:01 -0600

    docs: fix JSDoc

--- src/server/translationApi/getSimNamesAndTitles.js
+++ src/server/translationApi/getSimNamesAndTitles.js
@@ -11,7 +11,7 @@ import config from '../../common/config.js';
 import logger from './logger.js';
 
 /**
- * If the sim should be visible, extract it from the simulations object provided, otherwise return false.
+ * If the sim should be visible, extract its name from the simulations object provided, otherwise return false.
  *
  * @param {object} sim - simulations object from sim metadata
  * @returns {boolean|string} - false if sim shouldn't be visible, sim name otherwise
