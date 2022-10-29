5f218883fe5ad9b3f1330009222ca0e32812edcb
Liam Mulhall <liammulh@gmail.com>
2022-10-28 21:28:40 -0600

    refactor: remove unused modules, code

--- src/server/translationApi/api/simTitles.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2022, University of Colorado Boulder
-
-import getSimTitles from '../getSimTitles.js';
-
-const simTitles = async ( req, res ) => {
-  res.json( await getSimTitles() );
-};
-
-export default simTitles;
