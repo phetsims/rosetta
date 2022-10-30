8bae15849b904a5f336510482d74d38ea16cbe8b
Liam Mulhall <liammulh@gmail.com>
2022-10-30 16:19:43 -0600

    refactor: use shaOrBranch variable for getting string file

--- src/server/translationApi/getStringFileUrl.js
+++ src/server/translationApi/getStringFileUrl.js
@@ -13,15 +13,16 @@ import logger from './logger.js';
  * Return the URL of a repo's string file.
  *
  * @param {String} repoName - remote repository name; should be the same as a sim or library's name
+ * @param branchOrSha
  * @returns {String} - string file's URL
  */
-const getStringFileUrl = repoName => {
+const getStringFileUrl = ( repoName, branchOrSha = 'master' ) => {
   logger.verbose( `getting ${repoName}'s string file url` );
   logger.verbose( `got ${repoName}'s string file url; returning it` );
 
   // TODO: Change master to shaOrBranch variable.
   // TODO: Need SHA for published version of sim, otherwise deleted strings will be missing.
-  return `${config.GITHUB_URL}/${repoName}/master/${repoName}-strings_en.json`;
+  return `${config.GITHUB_URL}/${repoName}/${branchOrSha}/${repoName}-strings_en.json`;
 };
 
 export default getStringFileUrl;
