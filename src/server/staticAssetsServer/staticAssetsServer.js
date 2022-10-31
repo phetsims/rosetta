dbab1733e5b52b5edafca7497c72de3c1c51179a
Liam Mulhall <liammulh@gmail.com>
2022-10-30 20:14:42 -0600

    feat: make Rosetta work on Windows

--- src/server/staticAssetsServer/staticAssetsServer.js
+++ src/server/staticAssetsServer/staticAssetsServer.js
@@ -1,6 +1,7 @@
 // Copyright 2022, University of Colorado Boulder
 
 import express from 'express';
+import os from 'os';
 import { URL } from 'url';
 import path from 'path';
 
@@ -13,9 +14,18 @@ const __dirname = new URL( '.', import.meta.url ).pathname;
 // This is the path to the static files generated when you build the React front end.
 const staticAssetsPath = path.join( __dirname, '..', '..', 'client', 'dist' );
 
+// There's some weirdness on Windows; an extra slash is added before the path.
+const staticAssetsPathWithoutLeadingSlash = staticAssetsPath
+  .slice( 1, staticAssetsPath.length );
+
 // If we get any route, hand it over to the React app so that it can do client-side routing.
 staticAssetsServer.get( '/*', ( req, res ) => {
-  res.sendFile( path.join( staticAssetsPath, 'index.html' ) );
+  if ( os.platform() === 'win32' ) {
+    res.sendFile( path.join( staticAssetsPathWithoutLeadingSlash, 'index.html' ) );
+  }
+  else {
+    res.sendFile( path.join( staticAssetsPath, 'index.html' ) );
+  }
 } );
 
 export default staticAssetsServer;
