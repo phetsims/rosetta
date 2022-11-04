e942ef63e86a2d47d795487126b7d7d106c191e6
Liam Mulhall <liammulh@gmail.com>
2022-11-04 15:35:59 -0600

    build: add Gruntfile with update-rosetta task

--- Gruntfile.cjs
+++ Gruntfile.cjs
@@ -1,7 +1,83 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2022, University of Colorado Boulder
 
-/*
- * In Rosetta, we use npm to run things. See package.json for a comprehensive list of scripts that can be run.
+/**
+ * Define Grunt tasks for rosetta.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ * @author John Blanco
  */
 
-module.exports = require( '../chipper/js/grunt/Gruntfile.js' );
+const Gruntfile = require( '../chipper/js/grunt/Gruntfile' );
+const cloneRepo = require( '../perennial/js/common/cloneRepo.js' );
+const config = import('./src/common/config.js');
+const fs = require( 'fs' );
+const gitPull = require( '../perennial/js/common/gitPull' );
+const npmUpdate = require( '../perennial/js/common/npmUpdate.js' );
+const winston = require( 'winston' );
+
+const logger = winston.createLogger( {
+  format: winston.format.combine(
+    winston.format.simple()
+  ),
+  level: config.LOGGER_LEVEL,
+  transports: [
+    new winston.transports.Console()
+  ]
+} );
+
+module.exports = grunt => {
+  Gruntfile( grunt );
+
+  /**
+   * Rosetta depends on a parallel checkout of perennial, so we update perennial as part of updating rosetta.
+   * For the history of this, see:
+   * https://github.com/phetsims/phet-io/issues/1874#issuecomment-1233493616
+   *
+   * Basically, rosetta needs to use some code in perennial. We don't want to duplicate the code, so we are adding a
+   * dependency of a parallel perennial checkout.
+   */
+  grunt.registerTask( 'update-rosetta', 'This task pulls the latest versions of rosetta and its dependencies.', async function() {
+
+
+    // Tell Grunt this is an async task. Note how we use a regular function instead of an arrow function.
+    // See https://gruntjs.com/inside-tasks#this.async.
+    const done = this.async();
+    const errors = [];
+
+    const listOfReposToCloneOrPull = [ 'chipper', 'perennial', 'perennial-alias', 'rosetta' ];
+    for ( const repo of listOfReposToCloneOrPull ) {
+
+      // Try to clone repos.
+      if ( !fs.existsSync( `../${repo}` ) ) {
+        try {
+          await cloneRepo( repo );
+        }
+        catch( e ) {
+          logger.error( `issue with clone of ${repo}: ${e}` );
+          errors.push( e );
+        }
+      }
+
+      // Try to pull repos.
+      try {
+        const pullResult = await gitPull( repo );
+        logger.info( pullResult );
+      }
+      catch( e ) {
+        logger.error( `issue with git pull on ${repo}: ${e}` );
+        errors.push( e );
+      }
+
+      // Try to update repos.
+      try {
+        await npmUpdate( repo );
+      }
+      catch( e ) {
+        logger.error( `issue with npm update on ${repo}: ${e}` );
+        errors.push( e );
+      }
+    }
+
+    done( errors.length === 0 );
+  } );
+};
