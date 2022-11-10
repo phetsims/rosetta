3e4382abe9b65f11d162ca812c723314c41e4559
Liam Mulhall <liammulh@gmail.com>
2022-11-10 16:31:56 -0700

    fix: try different assets dir

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig( {
   plugins: [ react() ],
   build: {
-    sourcemap: true
+    sourcemap: true,
+    assetsDir: 'translate/assets'
   }
 } );
