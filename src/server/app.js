025178ec9a20ff31bf190af6b203e227d7742ccc
Liam Mulhall <liammulh@gmail.com>
2022-11-11 11:57:03 -0700

    refactor: rename variables to match new module name

--- src/server/app.js
+++ src/server/app.js
@@ -2,7 +2,7 @@
 
 /**
  * This file is the entry point for Rosetta's server-side code. It sets up the Express app, which has two components:
- * (1) the static file server, and
+ * (1) the built react app server, and
  * (2) the API server.
  *
  * (1) is responsible for serving the static files generated when we build the React front end. (2) is responsible for
@@ -33,27 +33,27 @@ app.use( express.json() );
 const __dirname = new URL( '.', import.meta.url ).pathname;
 
 // This is the path to the static files generated when you build the React front end.
-const staticAssetsPath = path.join( __dirname, '..', 'client', 'dist' );
+const builtReactFilesPath = path.join( __dirname, '..', 'client', 'dist' );
 
 // There's some weirdness on Windows; an extra slash is added before the path.
-const staticAssetsPathWithoutLeadingSlash = staticAssetsPath
-  .slice( 1, staticAssetsPath.length );
+const builtReactFilesPathWithoutLeadingSlash = builtReactFilesPath
+  .slice( 1, builtReactFilesPath.length );
 
 /*
- * We serve these static files using app as opposed to staticFileServer because if we use staticFileServer it tries to
- * serve all the files in the static directory as HTML instead of whatever their file type is, e.g. CSS or JS.
+ * We serve these static files using app as opposed to builtReactAppServer because if we use builtReactAppServer it
+ * tries to serve all the files in the static directory as HTML instead of whatever their file type is, e.g. CSS or JS.
  */
 if ( os.platform() === 'win32' ) {
-  app.use( express.static( staticAssetsPathWithoutLeadingSlash ) );
+  app.use( express.static( builtReactFilesPathWithoutLeadingSlash ) );
 }
 else {
-  app.use( express.static( staticAssetsPath ) );
+  app.use( express.static( builtReactFilesPath ) );
 }
 
 // Set up route for serving JSON data consumed by the React front end.
 app.use( '/translate/api', translationApi );
 
-// Set up route for serving the static files.
+// Set up route for serving the built React app files.
 app.use( '/translate', builtReactAppServer );
 
 // Mock website user data for local development.
