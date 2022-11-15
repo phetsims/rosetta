63700ceb106edd1c25f81a4dc2e78f6d664ba935
Liam Mulhall <liammulh@gmail.com>
2022-11-15 16:04:26 -0700

    feat: add sign out component

---
+++ src/client/components/SignOutLink.jsx
@@ -0,0 +1,11 @@
+// Copyright 2022, University of Colorado Boulder
+
+const SignOutLink = () => {
+  return (
+    <div>
+      <a href={`${window.location.origin}/en/sign-out`}>Sign out</a>
+    </div>
+  );
+};
+
+export default SignOutLink;
