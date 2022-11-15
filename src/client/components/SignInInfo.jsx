8dc6c75cc038d5999a287c46affee05395be5e5c
Liam Mulhall <liammulh@gmail.com>
2022-11-15 15:44:11 -0700

    feat: add sign in info component

---
+++ src/client/components/SignInInfo.jsx
@@ -0,0 +1,11 @@
+// Copyright 2022, University of Colorado Boulder
+
+const SignInInfo = () => {
+  return (
+    <div>
+      <p>Signed in as: foo@bar.com</p>
+    </div>
+  );
+};
+
+export default SignInInfo;
