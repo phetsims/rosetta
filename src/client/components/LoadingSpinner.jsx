c12de00ba62c9c02d27aaff10f88ad5072ab1ba1
Liam Mulhall <liammulh@gmail.com>
2022-11-17 13:55:07 -0700

    cleanup: LoadingSpinner.jsx

--- src/client/components/LoadingSpinner.jsx
+++ src/client/components/LoadingSpinner.jsx
@@ -1,5 +1,16 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Create a loading spinner.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
+/**
+ * This component displays a loading "spinner" (a little circle that loops, indicating something is loading).
+ *
+ * @returns {JSX.Element}
+ */
 const LoadingSpinner = () => {
   return (
     <div className='spinner-border text-primary' role='status'>
