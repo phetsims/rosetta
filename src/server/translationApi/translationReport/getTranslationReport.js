c96a29acc1d956e12a56cdd1fcbb658a43328157
Liam Mulhall <liammulh@gmail.com>
2022-11-18 13:57:14 -0700

    feat: add stub for getTranslationReport

---
+++ src/server/translationApi/translationReport/getTranslationReport.js
@@ -0,0 +1,22 @@
+// Copyright 2022, University of Colorado Boulder
+
+// import getLatestSimSha from '../getLatestSimSha.js';
+// import getEnglishStringKeysAndStrings from './getEnglishStringKeysAndStrings.js';
+// import getTranslatedAndUntranslatedSims from './getTranslatedAndUntranslatedSims.js';
+
+/**
+ * Return an object used for rendering the translation report for a given locale.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
+const getTranslationReport = async () => {
+
+  // const translatedAndUntranslatedSims = await getTranslatedAndUntranslatedSims( locale );
+  // for ( const sim of translatedAndUntranslatedSims.translated ) {
+  //   const latestSimSha = await getLatestSimSha( sim );
+  // }
+  return {};
+};
+
+export default getTranslationReport;
