8afc204224f1f665a8a960935d64e5e75d93d23d
Liam Mulhall <liammulh@gmail.com>
2022-11-18 23:54:38 -0700

    docs: update comments

--- src/server/translationApi/translationReport/getCommonTranslatedStringKeysAndValues.js
+++ src/server/translationApi/translationReport/getCommonTranslatedStringKeysAndValues.js
@@ -1,9 +1,6 @@
 // Copyright 2022, University of Colorado Boulder
 
 /**
- * Export a function that returns an array of arrays. Each sub-array has two elements: (1) a common translated string
- * key, and (2) the string corresponding to (1).
- *
  * @author Liam Mulhall
  */
 
@@ -14,10 +11,6 @@ import getTranslatedStringFileUrl from '../getTranslatedStringFileUrl.js';
 import logger from '../logger.js';
 
 /**
- * Return a list of ordered pairs where each ordered pair is a common translated string key followed by its value
- * (its string). This is implemented as an array of arrays where each sub-array has two elements, namely the common
- * translated string key and its string.
- *
  * @param {String} simName - sim name
  * @param {String} locale - two-letter ISO 639-1 locale code, e.g. es for Spanish
  * @param {String[]} simNames - list of all sim names
@@ -36,7 +29,7 @@ const getCommonTranslatedStringKeysAndValues = async (
   const commonTranslatedStringKeysAndStrings = new Map();
   const commonStringKeys = categorizedStringKeys.common;
 
-  // get a list of common repos for the sim
+  // Get a list of common repos for the sim.
   const commonRepos = await getCommonRepos( simName, simNames, stringKeysWithRepoName );
 
   /*
@@ -54,21 +47,21 @@ const getCommonTranslatedStringKeysAndValues = async (
    * keys for that repo.
    */
 
-  // create the above data structure
+  // Create the above data structure.
   const repoNameToStringKeys = getRepoNameToStringKeys( stringKeysWithRepoName, commonRepos, commonStringKeys );
 
-  // for each common repo from which the sim gets string keys...
+  // For each common repo from which the sim gets string keys...
   for ( const repo in repoNameToStringKeys ) {
 
-    // get the string file url
+    // Get the string file URL.
     const translatedStringFileUrl = getTranslatedStringFileUrl( repo, locale );
 
-    // try to get the contents stored at the string file url
+    // Try to get the contents stored at the string file URL.
     try {
       const translatedStringKeysAndStringsRes = await axios.get( translatedStringFileUrl );
       const translatedStringKeysAndStrings = translatedStringKeysAndStringsRes.data;
 
-      // for each string key associated with the repo, extract its value from the string file
+      // For each string key associated with the repo, extract its value from the string file.
       for ( const stringKey of repoNameToStringKeys[ repo ] ) {
         if ( translatedStringKeysAndStrings[ stringKey ] ) {
           commonTranslatedStringKeysAndStrings.set( stringKey, translatedStringKeysAndStrings[ stringKey ].value );
@@ -80,7 +73,7 @@ const getCommonTranslatedStringKeysAndValues = async (
     }
     catch( e ) {
 
-      // if there is no string file, then there hasn't been a translation that affects the repo
+      // If there is no string file, then there hasn't been a translation that affects the repo.
       if ( e.response.status === 404 ) {
         logger.verbose( `translated string file doesn't exist; setting empty strings for ${repo}` );
         for ( const stringKey of repoNameToStringKeys[ repo ] ) {
@@ -93,17 +86,17 @@ const getCommonTranslatedStringKeysAndValues = async (
     }
   }
 
-  // sort list according to the order of string keys in common categorized string keys
+  // Sort list according to the order of string keys in common categorized string keys.
   const sortedCommonTranslatedStringKeysAndStrings = new Map();
   for ( const stringKey of commonStringKeys ) {
     const stringValue = commonTranslatedStringKeysAndStrings.get( stringKey );
 
-    // if there is a value for the string key
+    // If there is a value for the string key...
     if ( stringValue ) {
       sortedCommonTranslatedStringKeysAndStrings.set( stringKey, stringValue );
     }
 
-    // if there isn't a value for the string key
+    // If there isn't a value for the string key...
     else {
       sortedCommonTranslatedStringKeysAndStrings.set( stringKey, '' );
     }
