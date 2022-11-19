b33d111e711c9e25096b0ac4ad20c5f8b2b358ee
Liam Mulhall <liammulh@gmail.com>
2022-11-18 23:42:47 -0700

    refactor: return object instead of array

--- src/server/translationApi/translationReport/getCommonEnglishStringKeysAndValues.js
+++ src/server/translationApi/translationReport/getCommonEnglishStringKeysAndValues.js
@@ -1,6 +1,8 @@
 // Copyright 2021-2022, University of Colorado Boulder
 
 /**
+ * Export a function that returns an object containing common English string keys and values for a given simulation.
+ *
  * @author Liam Mulhall
  */
 
@@ -9,17 +11,25 @@ import getCommonRepos from '../getCommonRepos.js';
 import getStringFileUrl from '../getStringFileUrl.js';
 import logger from '../logger.js';
 
+// import getCategorizedStringKeys from '../getCategorizedStringKeys.js';
+// import getSimHtml from '../getSimHtml.js';
+// import getSimMetadata from '../getSimMetadata.js';
+// import getSimNamesAndTitles from '../getSimNamesAndTitles.js';
+// import getSimUrl from '../getSimUrl.js';
+// import getStringKeysWithRepoName from '../getStringKeysWithRepoName.js';
+
 /**
+ * Return an object containing common English string keys and values for a given simulation
+ *
  * @param {String} simName - sim name
  * @param {String[]} simNames - list of all sim names
  * @param {{simSpecific: String[], common: String[]}} categorizedStringKeys - string keys categorized into common and sim-specific
  * @param {String[]} stringKeysWithRepoName - string keys with their respective repo names for the specified sim
- * @returns {Promise<String[][]>} - list of ordered pairs of common English string keys and strings
+ * @returns {Promise<Object>} - list of ordered pairs of common English string keys and strings
  */
 const getCommonEnglishStringKeysAndValues = async ( simName, simNames, categorizedStringKeys, stringKeysWithRepoName ) => {
   logger.info( `getting ${simName}'s common english string keys and values` );
   const commonEnglishStringKeysAndValues = {};
-  const stringKeysToCommonEnglishStrings = new Map();
   try {
 
     // For each common repo, get the string file.
@@ -42,7 +52,7 @@ const getCommonEnglishStringKeysAndValues = async ( simName, simNames, categoriz
         // Find the key in the file that matches the string key we're interested in.
         const matchingKey = Object.keys( stringFile ).find( key => key === stringKey );
         if ( stringFile[ matchingKey ] ) {
-          stringKeysToCommonEnglishStrings.set( stringKey, stringFile[ matchingKey ].value );
+          commonEnglishStringKeysAndValues[ stringKey ] = stringFile[ matchingKey ].value;
           stringKeyMapped = true;
         }
       }
@@ -53,7 +63,7 @@ const getCommonEnglishStringKeysAndValues = async ( simName, simNames, categoriz
 
         // We don't display unused string keys and strings to the user.
         // They get stripped out prior to sending them to the client.
-        stringKeysToCommonEnglishStrings.set( stringKey, 'no longer used' );
+        commonEnglishStringKeysAndValues[ stringKey ] = 'no longer used';
       }
     }
   }
@@ -66,5 +76,17 @@ const getCommonEnglishStringKeysAndValues = async ( simName, simNames, categoriz
   // Use spread operator and brackets to return an array.
   return commonEnglishStringKeysAndValues;
 };
+
+// ( async () => {
+//   const simName = 'acid-base-solutions';
+//   const simMetadata = await getSimMetadata();
+//   const simNames = Object.keys( getSimNamesAndTitles( simMetadata ) );
+//   const simUrl = getSimUrl( simName );
+//   const simHtml = await getSimHtml( simUrl );
+//   const stringKeysWithRepoName = getStringKeysWithRepoName( simHtml );
+//   const categorizedStringKeys = await getCategorizedStringKeys( simName, simNames, Object.keys( stringKeysWithRepoName ) );
+//   const res = await getCommonEnglishStringKeysAndValues( simName, simNames, categorizedStringKeys, Object.keys( stringKeysWithRepoName ) );
+//   console.log( JSON.stringify( res, null, 4 ) );
+// } )();
 
 export default getCommonEnglishStringKeysAndValues;
