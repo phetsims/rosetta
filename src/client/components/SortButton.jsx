18980c1dfb0cc1cd0a029471e7453a6d894a500d
Liam Mulhall <liammulh@gmail.com>
2022-11-30 14:33:35 -0700

    refactor: undo WIP on sortable table

--- src/client/components/SortButton.jsx
+++ src/client/components/SortButton.jsx
@@ -1,58 +1,11 @@
 // Copyright 2022, University of Colorado Boulder
 
-import { useEffect, useMemo, useState } from 'react';
 import arrowUpDown from '../img/arrow-down-up.svg';
 
-const ASCENDING_FLAG = 'ascending';
-const DESCENDING_FLAG = 'descending';
-
 const SortButton = props => {
-  const [ sortDirections, setSortDirections ] = useState( {
-    simTitle: null,
-    simSpecificPercent: null,
-    commonPercent: null
-  } );
-  const sortableReportObjects = props.reportObjects;
-  const sortedReportObjects = useMemo( () => {
-    if ( props.reportPopulated ) {
-      sortableReportObjects.sort( ( a, b ) => {
-        if ( a[ props.columnName ] < b[ props.columnName ] ) {
-          return sortDirections[ props.columnName ] === ASCENDING_FLAG ? -1 : 1;
-        }
-        if ( a[ props.columnName ] > b[ props.columnName ] ) {
-          return sortDirections[ props.columnName ] === ASCENDING_FLAG ? 1 : -1;
-        }
-        return 0;
-      } );
-    }
-    return sortableReportObjects;
-  }, [ props.columnName, sortDirections, sortableReportObjects ] );
-
-  useEffect( () => {
-    if ( props.reportPopulated ) {
-      props.setReportObjects( sortedReportObjects );
-    }
-  }, [ sortedReportObjects ] );
-
   const handleClick = () => {
     if ( props.reportPopulated ) {
-
-      // Set the direction of sorting.
-      if ( sortDirections[ props.columnName ] === null ) {
-        const newSortDirections = sortDirections;
-        newSortDirections[ props.columnName ] = ASCENDING_FLAG;
-        setSortDirections( newSortDirections );
-      }
-      else if ( sortDirections[ props.columnName ] === ASCENDING_FLAG ) {
-        const newSortDirections = sortDirections;
-        newSortDirections[ props.columnName ] = DESCENDING_FLAG;
-        setSortDirections( newSortDirections );
-      }
-      else if ( sortDirections[ props.columnName ] === DESCENDING_FLAG ) {
-        const newSortDirections = sortDirections;
-        newSortDirections[ props.columnName ] = ASCENDING_FLAG;
-        setSortDirections( newSortDirections );
-      }
+      window.alert( 'Sort it!' );
     }
     else {
       window.alert( 'Please wait until the report is populated before sorting a column.' );
