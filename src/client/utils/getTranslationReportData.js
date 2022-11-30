b471541b0625763306aea60a7c3bfaeffc4145c4
Liam Mulhall <liammulh@gmail.com>
2022-11-29 22:03:46 -0700

    refactor: get translation report data in separate module

---
+++ src/client/utils/getTranslationReportData.js
@@ -0,0 +1,29 @@
+// Copyright 2022, University of Colorado Boulder
+
+const getTranslationReportData = ( simNamesAndTitles, reportObjects, locale ) => {
+
+  const translationReportData = {};
+
+  // Initially, set all data to null.
+  for ( const simName of Object.keys( simNamesAndTitles ) ) {
+    translationReportData[ simName ] = {
+      simTitle: simNamesAndTitles[ simName ],
+      simSpecificPercentage: null,
+      commonPercentage: null
+    };
+  }
+
+  // Overwrite rows for which we have data.
+  for ( const reportObject of reportObjects ) {
+    const simSpecificPercent = Math.floor( ( reportObject.numSimSpecificTranslatedStrings / reportObject.numSimSpecificStrings ) * 100 );
+    const commonPercent = Math.floor( ( reportObject.numCommonTranslatedStrings / reportObject.numCommonStrings ) * 100 );
+    if ( Object.keys( translationReportData ).includes( reportObject.simName ) ) {
+      translationReportData[ reportObject.simName ].simSpecificPercentage = simSpecificPercent;
+      translationReportData[ reportObject.simName ].commonPercentage = commonPercent;
+    }
+  }
+
+  return translationReportData;
+};
+
+export default getTranslationReportData;
