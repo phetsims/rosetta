0c18263abd759a466ffe5c34a5faa0a23793e81f
Liam Mulhall <liammulh@gmail.com>
2022-11-29 20:01:56 -0700

    feat: add WIP for loading report stats

--- src/client/utils/getTranslationReportRows.jsx
+++ src/client/utils/getTranslationReportRows.jsx
@@ -1,9 +1,11 @@
 // Copyright 2022, University of Colorado Boulder
 
 import { Link } from 'react-router-dom';
+import LoadingSpinner from '../components/LoadingSpinner.jsx';
 
-const getTranslationReportRows = ( reportObjects, locale ) => {
+const getTranslationReportRows = ( simNamesAndTitles, reportObjects, locale ) => {
   const translationReportRows = [];
+  let lastSimName = '';
   for ( const reportObject of reportObjects ) {
     const simSpecificPercent = Math.floor( ( reportObject.numSimSpecificTranslatedStrings / reportObject.numSimSpecificStrings ) * 100 );
     const commonPercent = Math.floor( ( reportObject.numCommonTranslatedStrings / reportObject.numCommonStrings ) * 100 );
@@ -12,6 +14,19 @@ const getTranslationReportRows = ( reportObjects, locale ) => {
         <td><Link to={`/translate/${locale}/${reportObject.simName}`}>{reportObject.simTitle}</Link></td>
         <td>{simSpecificPercent}% ({reportObject.numSimSpecificTranslatedStrings} of {reportObject.numSimSpecificStrings})</td>
         <td>{commonPercent}% ({reportObject.numCommonTranslatedStrings} of {reportObject.numCommonStrings})</td>
+      </tr>
+    ) );
+    lastSimName = reportObject.simName;
+  }
+
+  const simNames = Object.keys( simNamesAndTitles );
+  const indexOfLastSimName = simNames.indexOf( lastSimName );
+  for ( let i = indexOfLastSimName; i < simNames.length; i++ ) {
+    translationReportRows.push( (
+      <tr key={simNames[ i ]}>
+        <td><Link to={`/translate/${locale}/${simNames[ i ]}`}>{simNamesAndTitles[ simNames[ i ] ]}</Link></td>
+        <td><LoadingSpinner/></td>
+        <td><LoadingSpinner/></td>
       </tr>
     ) );
   }
