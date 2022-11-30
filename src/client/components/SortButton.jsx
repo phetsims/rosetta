1875c121913f414887148169ff3c7dedfdce50c9
Liam Mulhall <liammulh@gmail.com>
2022-11-30 15:42:07 -0700

    refactor: pass onClick to SortButton

--- src/client/components/SortButton.jsx
+++ src/client/components/SortButton.jsx
@@ -3,16 +3,8 @@
 import arrowUpDown from '../img/arrow-down-up.svg';
 
 const SortButton = props => {
-  const handleClick = () => {
-    if ( props.reportPopulated ) {
-      window.alert( 'Sort it!' );
-    }
-    else {
-      window.alert( 'Please wait until the report is populated before sorting a column.' );
-    }
-  };
   return (
-    <button className='btn btn-light' onClick={handleClick}>
+    <button className='btn btn-light' onClick={props.onClick}>
       <img src={arrowUpDown} alt='up down arrow icon'/>
     </button>
   );
