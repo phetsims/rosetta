22c184bfc7d007c3ff91c5218e101ecce0cb7343
Liam Mulhall <liammulh@gmail.com>
2022-12-04 11:41:37 -0700

    refactor: put translated sims in their own table

---
+++ src/client/components/TranslatedSimsTable.jsx
@@ -0,0 +1,101 @@
+// Copyright 2022, University of Colorado Boulder
+
+/* eslint-disable indent */
+
+import { useContext, useState } from 'react';
+import useTranslationReportObjects from '../hooks/useTranslationReportObjects.jsx';
+import clientConstants from '../utils/clientConstants.js';
+import getTranslationReportRows from '../utils/getTranslationReportRows.jsx';
+import LoadingSpinner from './LoadingSpinner.jsx';
+import { SimNamesAndTitlesContext } from './RosettaRoutes.jsx';
+import SortButton from './SortButton.jsx';
+
+const TranslatedSimsTable = ( { locale } ) => {
+
+  const simNamesAndTitles = useContext( SimNamesAndTitlesContext );
+
+  // If you want to only see a few rows of stats for debugging, set this variable.
+  // WARNING: This should be set to null in production!
+  const numberOfEvents = clientConstants.numberOfShortReportEvents;
+
+  // Get report objects consisting of translation report data.
+  const { reportPopulated, reportObjects } = useTranslationReportObjects( locale, numberOfEvents );
+
+  // State variables used in sorting the table.
+  const [ sortKey, setSortKey ] = useState( 'simTitle' );
+  const [ sortDirection, setSortDirection ] = useState( 'ascending' );
+
+  // Get JSX rows to populate the table.
+  const reportRows = getTranslationReportRows(
+    simNamesAndTitles,
+    reportObjects,
+    locale,
+    reportPopulated,
+    sortKey,
+    sortDirection,
+    numberOfEvents
+  );
+
+  return (
+    <div>
+      <h3>Sims not yet translated into {locale}</h3>
+      {reportPopulated
+       ? <p>The translation report is finished!</p>
+       : <><p>The translation report is being populated...</p><LoadingSpinner/></>}
+      <table className='table table-striped'>
+        <thead>
+        <tr>
+          <th>Sim Title
+            {
+              reportPopulated
+              ? <SortButton onClick={() => {
+                setSortKey( 'simTitle' );
+                if ( sortDirection === 'ascending' ) {
+                  setSortDirection( 'descending' );
+                }
+                else {
+                  setSortDirection( 'ascending' );
+                }
+              }}/>
+              : <></>
+            }
+          </th>
+          <th>Sim-Specific Strings
+            {
+              reportPopulated
+              ? <SortButton onClick={() => {
+                setSortKey( 'simSpecificPercent' );
+                if ( sortDirection === 'ascending' ) {
+                  setSortDirection( 'descending' );
+                }
+                else {
+                  setSortDirection( 'ascending' );
+                }
+              }}/>
+              : <></>
+            }
+          </th>
+          <th>Common Strings
+            {
+              reportPopulated
+              ? <SortButton onClick={() => {
+                setSortKey( 'commonPercent' );
+                if ( sortDirection === 'ascending' ) {
+                  setSortDirection( 'descending' );
+                }
+                else {
+                  setSortDirection( 'ascending' );
+                }
+              }}/>
+              : <></>
+            }
+          </th>
+        </tr>
+        </thead>
+        <tbody>{reportRows}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TranslatedSimsTable;
--- src/client/components/TranslationReport.jsx
+++ src/client/components/TranslationReport.jsx
@@ -1,21 +1,15 @@
 // Copyright 2022, University of Colorado Boulder
 
-/* eslint-disable indent */
-
 /**
  * We define the translation report for a given locale.
  *
  * @author Liam Mulhall
  */
 
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
-import useTranslationReportObjects from '../hooks/useTranslationReportObjects.jsx';
-import getTranslationReportRows from '../utils/getTranslationReportRows.jsx';
-import LoadingSpinner from './LoadingSpinner.jsx';
-import { LocaleInfoContext, SimNamesAndTitlesContext } from './RosettaRoutes.jsx';
-import SortButton from './SortButton.jsx';
-import clientConstants from '../utils/clientConstants.js';
+import { LocaleInfoContext } from './RosettaRoutes.jsx';
+import TranslatedSimsTable from './TranslatedSimsTable.jsx';
 
 /**
  * This component allows a user to see a translation report for a given locale (statistics about translations) and
@@ -36,89 +30,11 @@ const TranslationReport = () => {
     localeName = localeInfo[ params.locale ].name;
   }
 
-  const simNamesAndTitles = useContext( SimNamesAndTitlesContext );
-
-  // If you want to only see a few rows of stats for debugging, set this variable.
-  // WARNING: This should be set to null in production!
-  const numberOfEvents = clientConstants.numberOfShortReportEvents;
-
-  // Get report objects consisting of translation report data.
-  const { reportPopulated, reportObjects } = useTranslationReportObjects( params.locale, numberOfEvents );
-
-  // State variables used in sorting the table.
-  const [ sortKey, setSortKey ] = useState( 'simTitle' );
-  const [ sortDirection, setSortDirection ] = useState( 'ascending' );
-
-  // Get JSX rows to populate the table.
-  const reportRows = getTranslationReportRows(
-    simNamesAndTitles,
-    reportObjects,
-    params.locale,
-    reportPopulated,
-    sortKey,
-    sortDirection,
-    numberOfEvents
-  );
-
   return (
     <div>
       <h1>Translation Report</h1>
       <h2 className='text-muted'>Locale: {localeName}</h2>
-      {reportPopulated
-       ? <p>The translation report is finished!</p>
-       : <><p>The translation report is being populated...</p><LoadingSpinner/></>}
-      <table className='table table-striped'>
-        <thead>
-        <tr>
-          <th>Sim Title
-            {
-              reportPopulated
-              ? <SortButton onClick={() => {
-                setSortKey( 'simTitle' );
-                if ( sortDirection === 'ascending' ) {
-                  setSortDirection( 'descending' );
-                }
-                else {
-                  setSortDirection( 'ascending' );
-                }
-              }}/>
-              : <></>
-            }
-          </th>
-          <th>Sim-Specific Strings
-            {
-              reportPopulated
-              ? <SortButton onClick={() => {
-                setSortKey( 'simSpecificPercent' );
-                if ( sortDirection === 'ascending' ) {
-                  setSortDirection( 'descending' );
-                }
-                else {
-                  setSortDirection( 'ascending' );
-                }
-              }}/>
-              : <></>
-            }
-          </th>
-          <th>Common Strings
-            {
-              reportPopulated
-              ? <SortButton onClick={() => {
-                setSortKey( 'commonPercent' );
-                if ( sortDirection === 'ascending' ) {
-                  setSortDirection( 'descending' );
-                }
-                else {
-                  setSortDirection( 'ascending' );
-                }
-              }}/>
-              : <></>
-            }
-          </th>
-        </tr>
-        </thead>
-        <tbody>{reportRows}</tbody>
-      </table>
+      <TranslatedSimsTable locale={params.locale}/>
     </div>
   );
 };
