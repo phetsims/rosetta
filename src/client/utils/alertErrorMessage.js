ff9140d16de16a41767b3a5fee18870282fd2a3b
Liam Mulhall <liammulh@gmail.com>
2022-12-06 10:55:34 -0700

    docs: add JSDoc

--- src/client/utils/alertErrorMessage.js
+++ src/client/utils/alertErrorMessage.js
@@ -1,5 +1,16 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Export a function that tells the user about an error.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
+/**
+ * Alert the user that an error occurred.
+ *
+ * @param error - the error message to be displayed
+ */
 const alertErrorMessage = error => {
   window.alert(
     `The translation utility experienced an error.
