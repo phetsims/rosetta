275a48f0fb167564c2b1f43b3909b412c472cd86
Liam Mulhall <liammulh@gmail.com>
2022-12-05 22:11:37 -0700

    docs: add JSDoc

--- src/client/components/SignOutLink.jsx
+++ src/client/components/SignOutLink.jsx
@@ -1,5 +1,17 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Create a component with a sign-out link.
+ *
+ * @author <liammulh@gmail.com>
+ */
+
+/**
+ * Return a re-usable sign-out link. (Easier to have the component than typing out the whole URL every time
+ * you need it.)
+ *
+ * @returns {JSX.Element}
+ */
 const SignOutLink = () => {
   return <a href={`${window.location.origin}/en/sign-out`}>Sign Out</a>;
 };
