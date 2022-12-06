e161dde98ef3d0134739caea0ba6de641888035b
Liam Mulhall <liammulh@gmail.com>
2022-12-05 21:20:20 -0700

    docs: add JSDoc

--- src/client/components/Sha.jsx
+++ src/client/components/Sha.jsx
@@ -1,10 +1,22 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Create a component that displays the SHA of the running instance of Rosetta. This should only be seen by
+ * team members.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Xerxes from '../img/xerxes.jpg';
 import clientConstants from '../utils/clientConstants.js';
 
+/**
+ * Get the SHA of the running instance of Rosetta from the server and display it.
+ *
+ * @returns {JSX.Element}
+ */
 const Sha = () => {
   const [ sha, setSha ] = useState( '' );
   useEffect( async () => {
