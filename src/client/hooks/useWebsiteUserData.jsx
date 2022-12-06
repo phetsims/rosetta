079cce646392729eeb078fc890fe98b4284c55fe
Liam Mulhall <liammulh@gmail.com>
2022-12-06 10:51:13 -0700

    docs: add JSDoc, add comment

--- src/client/hooks/useWebsiteUserData.jsx
+++ src/client/hooks/useWebsiteUserData.jsx
@@ -1,15 +1,31 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Create a custom hook for getting website user data.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import alertErrorMessage from '../utils/alertErrorMessage.js';
 
+/**
+ * Return the website user data we get from the PhET website.
+ *
+ * @returns {Object} - website user data, including whether someone is logged in, a trusted translator, or a team
+ *                     member.
+ */
 const useWebsiteUserData = () => {
-
   const [ websiteUserData, setWebsiteUserData ] = useState( {} );
-
-  // get website user data
   useEffect( async () => {
     try {
+
+      // We get the client config from the server because we need an easy way to tell
+      // whether we're in production or development. We need this info because the
+      // website's check-login service URL is undergoing some flux, and we need to be
+      // able to set it dynamically based on where Rosetta is deployed. For the website's
+      // check-login service URLs, see the server module where we define the client config.
       const clientConfigRes = await axios.get( '/translate/api/clientConfig' );
       const clientConfig = clientConfigRes.data;
       const websiteUserDataRes = await axios.get(
@@ -19,7 +35,7 @@ const useWebsiteUserData = () => {
       setWebsiteUserData( websiteUserDataRes.data );
     }
     catch( e ) {
-      console.error( e );
+      alertErrorMessage( e );
     }
   }, [] );
   return websiteUserData;
