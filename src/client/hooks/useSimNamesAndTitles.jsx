8fc61ee0265733c40da0dd6d6bd90517d5ba6143
Liam Mulhall <liammulh@gmail.com>
2022-12-06 10:26:05 -0700

    docs/fix: add JSDoc and improved error handling

--- src/client/hooks/useSimNamesAndTitles.jsx
+++ src/client/hooks/useSimNamesAndTitles.jsx
@@ -1,9 +1,21 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Create a custom hook for getting sim names and titles from the backend.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import alertErrorMessage from '../utils/alertErrorMessage.js';
 import clientConstants from '../utils/clientConstants.js';
 
+/**
+ * Get sims names and titles from the backend and return them. Show an error message if the request fails.
+ *
+ * @returns {Object} - an object with sim names as keys and sim titles as values
+ */
 const useSimNamesAndTitles = () => {
 
   const [ simNamesAndTitles, setSimNamesAndTitles ] = useState( [] );
@@ -13,7 +25,7 @@ const useSimNamesAndTitles = () => {
       setSimNamesAndTitles( simTitlesRes.data );
     }
     catch( e ) {
-      console.error( e );
+      alertErrorMessage( e );
     }
   }, [] );
   return simNamesAndTitles;
