fb4e90a151434331defde25f3711d0a6404931f9
Liam Mulhall <liammulh@gmail.com>
2022-12-06 10:20:31 -0700

    docs: add JSDoc

--- src/client/hooks/useLocaleInfo.jsx
+++ src/client/hooks/useLocaleInfo.jsx
@@ -1,14 +1,23 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Create a custom react hook for getting locale info from the backend.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import alertErrorMessage from '../utils/alertErrorMessage.js';
 import clientConstants from '../utils/clientConstants.js';
 
+/**
+ * Get the locale info from the backend and return it. If the request fails, show an error message.
+ *
+ * @returns {Object} - locale info with locale codes and locale names
+ */
 const useLocaleInfo = () => {
   const [ localeInfo, setLocaleInfo ] = useState( {} );
-
-  // get locale info
   useEffect( async () => {
     try {
       const localeInfoRes = await axios.get( `${clientConstants.translationApiRoute}/localeInfo` );
