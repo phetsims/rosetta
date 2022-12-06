3e8f46c59e896546b82c1122537c1b464e330cf9
Liam Mulhall <liammulh@gmail.com>
2022-12-05 22:30:19 -0700

    docs: add JSDoc

--- src/client/components/SortButton.jsx
+++ src/client/components/SortButton.jsx
@@ -1,7 +1,19 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Create a button with a sort icon.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import arrowUpDown from '../img/arrow-down-up.svg';
 
+/**
+ * The sort button has an up-down arrow icon.
+ *
+ * @param props
+ * @returns {JSX.Element}
+ */
 const SortButton = props => {
   return (
     <button className='btn btn-light' onClick={props.onClick}>
