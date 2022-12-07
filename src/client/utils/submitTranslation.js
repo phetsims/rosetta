e7504a1631775dde0b67095feba07658f58201ba
Liam Mulhall <liammulh@gmail.com>
2022-12-07 13:42:51 -0700

    docs: add JSDoc

--- src/client/utils/submitTranslation.js
+++ src/client/utils/submitTranslation.js
@@ -1,10 +1,23 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Submit a translation for publication to the PhET website.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import axios from 'axios';
 import alertErrorMessage from './alertErrorMessage.js';
 import clientConstants from './clientConstants.js';
 import makeTranslationObject from './makeTranslationObject.js';
 
+/**
+ * Issue a post request to submit a translation for publication to the PhET website.
+ *
+ * @param {Object} values - the values in the translation form
+ * @param {String} simName - the name of the sim being translated
+ * @param {String} locale - the locale code of the sim being translated
+ */
 const submitTranslation = async ( values, simName, locale ) => {
   const translation = await makeTranslationObject( values, simName, locale );
   if ( window.confirm( `Are you sure you want to submit your translation for ${translation.simName} in locale ${translation.locale}?` ) ) {
