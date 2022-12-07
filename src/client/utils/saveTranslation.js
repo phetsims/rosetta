f5cc677684bf198036c3a94b63b9a2966bb5e51d
Liam Mulhall <liammulh@gmail.com>
2022-12-07 13:39:17 -0700

    docs: add JSDoc

--- src/client/utils/saveTranslation.js
+++ src/client/utils/saveTranslation.js
@@ -1,10 +1,23 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Save a translation to short-term string storage.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import axios from 'axios';
 import alertErrorMessage from './alertErrorMessage.js';
 import clientConstants from './clientConstants.js';
 import makeTranslationObject from './makeTranslationObject.js';
 
+/**
+ * Issue a post request to save a translation to the short-term string storage database.
+ *
+ * @param {Object} values - the values in the translation form
+ * @param {String} simName - the name of the sim
+ * @param {String} locale - the locale code
+ */
 const saveTranslation = async ( values, simName, locale ) => {
   const translation = await makeTranslationObject( values, simName, locale );
   if ( window.confirm( `If you have a translation saved for ${translation.simName} in locale ${translation.locale}, it will be overwritten.` ) ) {
