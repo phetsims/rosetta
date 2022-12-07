b7a1478195b21492c1319e45d2290a9444967dfd
Liam Mulhall <liammulh@gmail.com>
2022-12-07 14:12:43 -0700

    docs: update JSDoc

--- src/server/translationApi/api/rebuildWithOriginalCredit.js
+++ src/server/translationApi/api/rebuildWithOriginalCredit.js
@@ -1,10 +1,17 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Request a rebuild for a sim with the original submitter's user ID.
+ * This is what we used to call "trigger build".
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import logger from '../logger.js';
 import requestRebuildWithOriginalCredit from '../rebuildWithOriginalCredit/requestRebuildWithOriginalCredit.js';
 
 /**
- * API function. Respond with the current SHA of the running instance of Rosetta.
+ * API function. Request a rebuild of the sim with the correct user ID.
  *
  * @param {Object} req - the Express request object
  * @param {Object} res - the Express response object
