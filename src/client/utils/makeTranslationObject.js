8be501eab5334bb89e5a197e73d6c8809a492192
Liam Mulhall <liammulh@gmail.com>
2022-12-07 13:12:00 -0700

    docs: add JSDoc

--- src/client/utils/makeTranslationObject.js
+++ src/client/utils/makeTranslationObject.js
@@ -1,8 +1,23 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Create the translation object we send to the server when the user submits their translation for publication to the
+ * PhET website.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import alertErrorMessage from './alertErrorMessage.js';
 import getWebsiteUserData from './getWebsiteUserData.js';
 
+/**
+ * Return an object with the data needed for submitting the sim for publication.
+ *
+ * @param {Object} values - values we get from the translation form
+ * @param {String} simName - name of the sim being translated
+ * @param {String} locale - locale code of the sim being translated
+ * @returns {Promise<Object>} - object with data necessary for submission
+ */
 const makeTranslationObject = async ( values, simName, locale ) => {
   let translation;
   try {
