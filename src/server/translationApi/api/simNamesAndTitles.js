b37edb793281b1f6e143a4c4a3a92d6620b490d7
Liam Mulhall <liammulh@gmail.com>
2022-12-07 14:20:36 -0700

    docs: add JSDoc

--- src/server/translationApi/api/simNamesAndTitles.js
+++ src/server/translationApi/api/simNamesAndTitles.js
@@ -1,8 +1,20 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Define functionality for the sim names and titles route.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import getSimMetadata from '../getSimMetadata.js';
 import getSimNamesAndTitles from '../getSimNamesAndTitles.js';
 
+/**
+ * Respond with an object whose keys are sim names and whose values are sim titles.
+ *
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
 const simNamesAndTitles = async ( req, res ) => {
   res.json( getSimNamesAndTitles( await getSimMetadata() ) );
 };
