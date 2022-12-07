be5ba47f7d3e4f66b7bd5e66c798d635a01e32f6
Liam Mulhall <liammulh@gmail.com>
2022-12-07 13:51:46 -0700

    docs: add JSDoc

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -1,5 +1,11 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * This is the configuration file for Vite, the transpiler/bundler we use.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
