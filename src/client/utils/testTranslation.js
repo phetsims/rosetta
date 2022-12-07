9c74c56844c1168c42fe267b1c4e5ea0b77881c0
Liam Mulhall <liammulh@gmail.com>
2022-12-07 13:45:34 -0700

    docs: add JSDoc

--- src/client/utils/testTranslation.js
+++ src/client/utils/testTranslation.js
@@ -1,10 +1,24 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Open the sim in a new window with the strings the user has inputted.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import axios from 'axios';
 import alertErrorMessage from './alertErrorMessage.js';
 import clientConstants from './clientConstants.js';
 import makeTranslationObject from './makeTranslationObject.js';
 
+/**
+ * Use the backend to replace the sim's strings with the user's inputted strings, then open a new tab with the sim with
+ * the replaced strings.
+ *
+ * @param {Object} values - translation form values
+ * @param {String} simName - name of the sim
+ * @param {String} locale - locale code for the sim
+ */
 const testTranslation = async ( values, simName, locale ) => {
   const translation = await makeTranslationObject( values, simName, locale );
   try {
