0199386f17ede31b58808ee2d69c79d327dc36d7
Liam Mulhall <liammulh@gmail.com>
2022-12-14 12:13:59 -0700

    docs: add JSDoc

--- src/client/hooks/useTranslatedAndUntranslatedSims.jsx
+++ src/client/hooks/useTranslatedAndUntranslatedSims.jsx
@@ -1,10 +1,22 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Get object containing a list of translated sims and a list of untranslated sims for a given locale.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import alertErrorMessage from '../js/alertErrorMessage.js';
 import publicConfig from '../../common/publicConfig.js';
 
+/**
+ * Return an object containing a list of translated sims and a list of untranslated sims for the given locale.
+ *
+ * @param locale
+ * @returns {Object}
+ */
 const useTranslatedAndUntranslatedSims = locale => {
   const [ translatedAndUntranslatedSims, setTranslatedAndUntranslatedSims ] = useState( null );
   useEffect( async () => {
