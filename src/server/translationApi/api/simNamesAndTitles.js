5c6370efc463a4dc77c8355f49eec7afc5d90970
Liam Mulhall <liammulh@gmail.com>
2022-12-14 13:03:49 -0700

    fix: add log statement

--- src/server/translationApi/api/simNamesAndTitles.js
+++ src/server/translationApi/api/simNamesAndTitles.js
@@ -8,6 +8,7 @@
 
 import getSimMetadata from '../getSimMetadata.js';
 import getSimNamesAndTitles from '../getSimNamesAndTitles.js';
+import logger from '../logger.js';
 
 /**
  * Respond with an object whose keys are sim names and whose values are sim titles.
@@ -16,6 +17,7 @@ import getSimNamesAndTitles from '../getSimNamesAndTitles.js';
  * @param {Object} res - Express response object
  */
 const simNamesAndTitles = async ( req, res ) => {
+  logger.info( 'responding with sim names and titles' );
   res.json( getSimNamesAndTitles( await getSimMetadata() ) );
 };
 
