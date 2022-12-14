3e98ef19565b37ae9873a337942695cb71adaae4
Liam Mulhall <liammulh@gmail.com>
2022-12-14 13:49:21 -0700

    docs: add JSDoc

--- src/server/translationApi/getSimVersionObject.js
+++ src/server/translationApi/getSimVersionObject.js
@@ -1,8 +1,20 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Export a function that gets the version object for a given sim.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
 import getSimMetadata from './getSimMetadata.js';
 import logger from './logger.js';
 
+/**
+ * Extract the version object for the given sim from the sim metadata and return it.
+ *
+ * @param simName - name of the sim
+ * @returns {Promise<string>}
+ */
 const getSimVersionObject = async simName => {
   let latestVersionOfSim = '';
   try {
