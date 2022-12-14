db712068b04f872aadec63132fd064774977574a
Liam Mulhall <liammulh@gmail.com>
2022-12-14 11:36:52 -0700

    docs: add JSDoc

--- src/client/components/TranslationFormHeader.jsx
+++ src/client/components/TranslationFormHeader.jsx
@@ -1,5 +1,19 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * Create a header that displays info about a user's translation.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
+/**
+ * Display info about the sim/locale the user is translating.
+ *
+ * @param localeName
+ * @param locale
+ * @param simTitle
+ * @returns {JSX.Element}
+ */
 const TranslationFormHeader = ( { localeName, locale, simTitle } ) => {
   return <h2>Translating {localeName} ({locale}) {simTitle}</h2>;
 };
