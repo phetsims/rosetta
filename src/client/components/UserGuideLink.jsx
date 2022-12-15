56b1b93341b89dd8eaf91ef8c87520d571e739f6
Liam Mulhall <liammulh@gmail.com>
2022-12-15 16:14:00 -0700

    feat: open user guide in new tab
    Part of addressing feedback in
    https://github.com/phetsims/rosetta/issues/328#issuecomment-1353826009.

--- src/client/components/UserGuideLink.jsx
+++ src/client/components/UserGuideLink.jsx
@@ -13,7 +13,7 @@
  */
 const UserGuideLink = () => {
   return (
-    <a className='nav-link' href='https://docs.google.com/document/d/e/2PACX-1vSZz9K1ris-dRNsLK9ZoG_KtMce1fJ1mlz-NDdk0KVB7Sfk_m18HmOaUKzb_zh6eFSsUwR1x6LCenLR/pub?embedded=true'>
+    <a target='_blank' rel='noreferrer' className='nav-link' href='https://docs.google.com/document/d/e/2PACX-1vSZz9K1ris-dRNsLK9ZoG_KtMce1fJ1mlz-NDdk0KVB7Sfk_m18HmOaUKzb_zh6eFSsUwR1x6LCenLR/pub?embedded=true'>
       User Guide
     </a>
   );
