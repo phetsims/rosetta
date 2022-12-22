98229df55c8efd636855f35f46f6075c309af94a
Liam Mulhall <liammulh@gmail.com>
2022-12-22 12:11:38 -0700

    feat: use enum for sort keys
    Part of https://github.com/phetsims/rosetta/issues/332.
    
    Also part of https://github.com/phetsims/qa/issues/869.

---
+++ src/client/js/SortKeyEnum.js
@@ -0,0 +1,17 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * Export a sort key enum.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
+const SortKeyEnum = {
+  SIM_TITLE: 'simTitle',
+  SIM_SPECIFIC_PERCENT: 'simSpecificPercent',
+  COMMON_PERCENT: 'commonPercent'
+};
+
+Object.freeze( SortKeyEnum );
+
+export default SortKeyEnum;
