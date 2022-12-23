b22b0017ac4ca595a52040c2c25f0de645af32ad
Liam Mulhall <liammulh@gmail.com>
2022-12-23 10:34:29 -0700

    feat: use enum for sort direction
    Part of https://github.com/phetsims/rosetta/issues/332.
    
    Also part of https://github.com/phetsims/qa/issues/869.

---
+++ src/client/js/SortDirectionEnum.js
@@ -0,0 +1,16 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * Export a sort direction enum.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ */
+
+const SortDirectionEnum = {
+  ASCENDING: 'ascending',
+  DESCENDING: 'descending'
+};
+
+Object.freeze( SortDirectionEnum );
+
+export default SortDirectionEnum;
