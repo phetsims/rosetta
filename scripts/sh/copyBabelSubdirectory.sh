f111604695e20e60bffd23bd24600ca7a64c0793
Marla Schulz <marlaa.schulz@gmail.com>
2023-01-25 15:47:35 -0800

    Add duplicate babel strings shell script, see: https://github.com/phetsims/number-suite-common/issues/27

---
+++ scripts/sh/copyBabelSubdirectory.sh
@@ -0,0 +1,19 @@
+# Copyright 2022, University of Colorado Boulder
+#
+#
+# This file defines a bash script that can be used to duplicate a babel subdirectory. This was used for Number Play, when
+# translated strings had to be moved to common repos.
+#
+# @author Marla Schulz (PhET Interactive Simulations)
+# @author Chris Klusendorf (PhET Interactive Simulations)
+# @author Michael Kauzmann (PhET Interactive Simulations)
+#
+
+
+find . -type f -name 'number-play-string' |
+while IFS= read file_name; do
+  echo "$file_name"
+
+  #21 characters before adding translation subscript.
+  mv "$file_name" "number-suite-common-strings${file_name:21}"
+done
