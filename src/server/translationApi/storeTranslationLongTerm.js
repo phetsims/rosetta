b953056c4c24689613a978e37493c494b24106fc
Liam Mulhall <liammulh@gmail.com>
2023-02-24 17:55:19 -0700

    fix: handle case where there isn't a translation file more gracefully
    For https://github.com/phetsims/rosetta/issues/377.

--- src/server/translationApi/storeTranslationLongTerm.js
+++ src/server/translationApi/storeTranslationLongTerm.js
@@ -31,12 +31,12 @@ const storeTranslationLongTerm = async preparedTranslation => {
 
       logger.info( `iterating over ${repo}` );
 
-      // check to see if the object is not empty (i.e. strings were translated in repo)
+      // Check to see if the object is not empty (i.e. strings were translated in repo).
       if ( Object.keys( contents[ repo ] ).length !== 0 ) {
 
         logger.info( `storing translation of strings in ${repo} long-term` );
 
-        // make the translation file contents a string and use base 64 encoding
+        // Make the translation file contents a string and use base 64 encoding.
         const fileContents = JSON.stringify( contents[ repo ], null, 2 );
         const translationFilePath = `${repo}/${repo}-strings_${preparedTranslation.locale}.json`;
         const commitMessage = `automated commit from rosetta for sim/lib ${repo}, locale ${preparedTranslation.locale}`;
@@ -44,6 +44,7 @@ const storeTranslationLongTerm = async preparedTranslation => {
 
           logger.info( 'trying to update contents of file on github' );
 
+          // Try to update the file.
           const contentsRes = await longTermStorage.contentsAsync( translationFilePath, privateConfig.BABEL_BRANCH );
           longTermStorageRes = await longTermStorage.updateContentsAsync(
             translationFilePath,
@@ -57,28 +58,37 @@ const storeTranslationLongTerm = async preparedTranslation => {
           logger.info( `stored translation of strings in ${repo} long-term` );
         }
         catch( e ) {
-          logger.error( e );
-          try {
+          if ( e.statusCode === 404 ) {
 
-            // try to create the file
-            logger.info( 'trying to create file on github' );
-            longTermStorageRes = await longTermStorage.createContentsAsync(
-              translationFilePath,
-              commitMessage,
-              fileContents,
-              privateConfig.BABEL_BRANCH
-            );
-            logger.info( 'created file on github' );
-            logger.info( `stored translation of strings in ${repo} long-term` );
+            logger.info( `file not found for ${preparedTranslation.locale}/${preparedTranslation.simName}` );
+
+            try {
+
+              // Try to create the file.
+              logger.info( 'trying to create file on github' );
+              longTermStorageRes = await longTermStorage.createContentsAsync(
+                translationFilePath,
+                commitMessage,
+                fileContents,
+                privateConfig.BABEL_BRANCH
+              );
+              console.log( `----------> longTermStorageRes = ${JSON.stringify( longTermStorageRes, null, 4 )}` );
+              logger.info( 'created file on github' );
+              logger.info( `stored translation of strings in ${repo} long-term` );
+            }
+            catch( e ) {
+              logger.error( 'unable to create file' );
+              logger.error( e );
+            }
           }
-          catch( e ) {
+          else {
             logger.error( e );
           }
         }
       }
     }
   }
-  logger.info( `translation was stored long-term: ${longTermStorageRes}` );
+  logger.info( `storage response not null: ${longTermStorageRes !== null}` );
   logger.info( 'done attempting to store translation long term' );
   return longTermStorageRes;
 };
