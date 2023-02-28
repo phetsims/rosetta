6e364d5f64e5c02cdbb59a00251290ec416a6399
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-02-28 15:37:43 -0700

    update/simplify/improve script to remove translated string keys that are not in the en string, https://github.com/phetsims/my-solar-system/issues/87

--- scripts/js/remove-babel-duplicate-strings.js
+++ scripts/js/remove-babel-duplicate-strings.js
@@ -1,7 +1,16 @@
 // Copyright 2022, University of Colorado Boulder
 
 /**
- * NodeJS script for removing strings that are duplicated in babel subdirectories.
+ * NodeJS script for removing strings that are duplicated in babel subdirectories. This looks at the en json, and will
+ * remove any keys in babel translated files that are not in the en string files. Be careful, this is an atypical script
+ * that has only yet been useful when a sim is split into a mini-suite with a common repo
+ * (number-suite-common,solar-system-common).
+ *
+ * Good luck!
+ *
+ * usage:
+ * cd root/
+ * node rosetta/scripts/js/remove-babel-duplicate-strings.js my-repo
  *
  * @author Chris Klusendorf (PhET Interactive Simulations)
  * @author Marla Schulz (PhET Interactive Simulations)
@@ -9,36 +18,37 @@
 
 // modules
 const fs = require( 'fs' );
+const assert = require( 'assert' );
 
-const REPOS_WITH_STRING_DUPLICATES = [
-  'number-play',
-  'number-compare',
-  'number-suite-common'
-];
+const repoName = process.argv[ 2 ];
 
-REPOS_WITH_STRING_DUPLICATES.forEach( repoName => {
+assert( typeof repoName === 'string', 'repo arg required' );
 
-  // read the string file and turn it into an object
-  const enStringFile = fs.readFileSync( `./${repoName}/${repoName}-strings_en.json`, 'utf8' );
-  const enStringKeys = Object.keys( JSON.parse( enStringFile ) );
+// read the string file and turn it into an object
+const enStringFile = fs.readFileSync( `./${repoName}/${repoName}-strings_en.json`, 'utf8' );
+const enStringKeys = Object.keys( JSON.parse( enStringFile ) );
 
-  console.log( `\nlooking for duplicates in ${repoName}\n` );
+console.log( `\nlooking for duplicates in ${repoName}\n` );
 
-  const files = fs.readdirSync( `./babel/${repoName}` );
+const files = fs.readdirSync( `./babel/${repoName}` );
 
-  // iterate through all translated string files for repoName in babel
-  files.forEach( fileName => {
+// iterate through all translated string files for repoName in babel
+files.forEach( fileName => {
 
-    const translatedStringsFileName = `./babel/${repoName}/${fileName}`;
-    const translatedStrings = JSON.parse( fs.readFileSync( translatedStringsFileName, 'utf8' ) );
+  const translatedStringsFileName = `./babel/${repoName}/${fileName}`;
+  const translatedStrings = JSON.parse( fs.readFileSync( translatedStringsFileName, 'utf8' ) );
 
-    // remove the data of any translated string that's not found in the english strings
-    for ( const key in translatedStrings ) {
-      !enStringKeys.includes( key ) && delete translatedStrings[ key ];
+  // remove the data of any translated string that's not found in the english strings
+  for ( const key in translatedStrings ) {
+
+    if ( !enStringKeys.includes( key ) ) {
+      console.log( 'deleting', key, fileName );
+      delete translatedStrings[ key ];
     }
+  }
 
-    console.log( `writing new file: ${fileName}` );
-    fs.writeFileSync( translatedStringsFileName, JSON.stringify( translatedStrings, null, 2 ) );
-  } );
+  assert( Object.keys( translatedStrings ).length <= enStringKeys.length, 'something went wrong, there are too many translated keys.' );
 
+  console.log( `writing new file: ${fileName}` );
+  fs.writeFileSync( translatedStringsFileName, JSON.stringify( translatedStrings, null, 2 ) );
 } );
