7f0b07697a7186f70cf9aeb72fc9f63d0229c866
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-10-15 12:08:52 -0600

    move to new grunt entrypoint pattern in rosetta, https://github.com/phetsims/chipper/issues/1464

--- Gruntfile.cjs
+++ Gruntfile.cjs
@@ -9,73 +9,9 @@
 'use strict';
 
 const Gruntfile = require( '../chipper/Gruntfile' );
-const cloneRepo = require( '../perennial/js/common/cloneRepo.js' );
-const fs = require( 'fs' );
-const gitPull = require( '../perennial/js/common/gitPull' );
-const npmUpdate = require( '../perennial/js/common/npmUpdate.js' );
-const winston = require( 'winston' );
+const registerTasks = require( '../perennial/js/grunt/util/registerTasks' );
 
-const REPO_DEPENDENCIES = [ 'chipper', 'perennial', 'perennial-alias' ];
-
-const logger = winston.createLogger( {
-  format: winston.format.combine(
-    winston.format.simple()
-  ),
-  level: 'info',
-  transports: [
-    new winston.transports.Console()
-  ]
-} );
-
-module.exports = grunt => {
-  Gruntfile( grunt );
-
-  /**
-   * Rosetta depends on a parallel checkout of perennial, perennial-alias, and chipper, so we update these repos
-   * as part of updating rosetta. For the history of this, see:
-   * https://github.com/phetsims/phet-io/issues/1874#issuecomment-1233493616
-   *
-   * This task is intended to be used when you are pulling rosetta onto the production server or the test server.
-   */
-  grunt.registerTask( 'update-rosetta', 'This task pulls the latest versions of rosetta and its dependencies.', async function() {
-
-    // Tell Grunt this is an async task. Note how we use a regular function instead of an arrow function.
-    // See https://gruntjs.com/inside-tasks#this.async.
-    const done = this.async();
-    const errors = [];
-
-    for ( const repo of REPO_DEPENDENCIES ) {
-
-      // Try to clone repos.
-      if ( !fs.existsSync( `../${repo}` ) ) {
-        try {
-          await cloneRepo( repo );
-        }
-        catch( e ) {
-          logger.error( `issue with cloning ${repo}: ${e}` );
-        }
-      }
-
-      // Try to pull repos.
-      try {
-        const pullResult = await gitPull( repo );
-        logger.info( pullResult );
-      }
-      catch( e ) {
-        logger.error( `issue with git pull on ${repo}: ${e}` );
-        errors.push( e );
-      }
-
-      // Try to update repos.
-      try {
-        await npmUpdate( repo );
-      }
-      catch( e ) {
-        logger.error( `issue with npm update on ${repo}: ${e}` );
-        errors.push( e );
-      }
-    }
-
-    done( errors.length === 0 );
-  } );
+module.exports = function( grunt ) {
+  Gruntfile( grunt ); // use chipper's gruntfile
+  registerTasks( grunt, `${__dirname}/grunt/tasks/` );
 };
---
+++ grunt/tasks/update-rosetta.cjs
@@ -0,0 +1,63 @@
+// Copyright 2022, University of Colorado Boulder
+
+/**
+ * Rosetta depends on a parallel checkout of perennial, perennial-alias, and chipper, so we update these repos
+ * as part of updating rosetta. For the history of this, see:
+ * https://github.com/phetsims/phet-io/issues/1874#issuecomment-1233493616
+ *
+ * This task is intended to be used when you are pulling rosetta onto the production server or the test server.
+ *
+ * @author Liam Mulhall <liammulh@gmail.com>
+ * @author John Blanco
+ */
+'use strict';
+
+const cloneRepo = require( '../../../perennial/js/common/cloneRepo.js' );
+const fs = require( 'fs' );
+const gitPull = require( '../../../perennial/js/common/gitPull' );
+const npmUpdate = require( '../../../perennial/js/common/npmUpdate.js' );
+const winston = require( 'winston' );
+
+const REPO_DEPENDENCIES = [ 'chipper', 'perennial', 'perennial-alias' ];
+
+const logger = winston.createLogger( {
+  format: winston.format.combine(
+    winston.format.simple()
+  ),
+  level: 'info',
+  transports: [
+    new winston.transports.Console()
+  ]
+} );
+
+( async function() {
+  for ( const repo of REPO_DEPENDENCIES ) {
+
+    // Try to clone repos.
+    if ( !fs.existsSync( `../${repo}` ) ) {
+      try {
+        await cloneRepo( repo );
+      }
+      catch( e ) {
+        logger.error( `issue with cloning ${repo}: ${e}` );
+      }
+    }
+
+    // Try to pull repos.
+    try {
+      const pullResult = await gitPull( repo );
+      logger.info( pullResult );
+    }
+    catch( e ) {
+      logger.error( `issue with git pull on ${repo}: ${e}` );
+    }
+
+    // Try to update repos.
+    try {
+      await npmUpdate( repo );
+    }
+    catch( e ) {
+      logger.error( `issue with npm update on ${repo}: ${e}` );
+    }
+  }
+} )();
