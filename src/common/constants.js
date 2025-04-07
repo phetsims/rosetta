b56a5e585e79e0058f8287ee824e5a3a35d42575
AgustinVallejo <agustin.vallejo@udea.edu.co>
2025-04-07 14:58:34 -0500

    Add validation for triple braces in translations, see https://github.com/phetsims/rosetta/issues/424

--- src/common/constants.js
+++ src/common/constants.js
@@ -28,6 +28,7 @@ export const TRANSLATION_API_ROUTE = publicConfig.ENVIRONMENT === 'development' 
 // have placeholder patterns.
 export const SINGLE_BRACE_REGEX = /\{\d+\}/g;
 export const DOUBLE_BRACE_REGEX = /\{\{\w+\}\}/g;
+export const TRIPLE_BRACE_REGEX = /\{\{\{\w+\}\}\}/g;
 
 // This is the object in the published sim HTML that contains the REPO_NAME/stringKey pairs.
 export const STRING_KEYS_WITH_REPO_NAME_IN_HTML_VAR = 'window.phet.chipper.strings';
