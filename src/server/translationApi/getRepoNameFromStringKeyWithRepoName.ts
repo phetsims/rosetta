60710cc0dbec85e23428f831681c8f710ac683b2
jbphet <jpblanco@yahoo.com>
2025-04-16 17:24:19 -0600

    continue conversion to TypeScript, see https://github.com/phetsims/rosetta/issues/311

--- src/server/translationApi/getRepoNameFromStringKeyWithRepoName.ts
+++ src/server/translationApi/getRepoNameFromStringKeyWithRepoName.ts
@@ -13,13 +13,18 @@ import logger from './logger.js';
  * Return a lowercase-kebab (repo-style) repo name (e.g. acid-base-solutions) from a string that looks like
  * stringKey/REPO_NAME. We get the strings that look like REPO_NAME/stringKey from a sim's HTML.
  *
- * @param {String} stringKeyWithRepoName - stringKey/REPO_NAME we get from a sim's HTML
- * @returns {string} - lowercase-kebab (repo-style) repo name (e.g. acid-base-solutions)
+ * @param stringKeyWithRepoName - stringKey/REPO_NAME we get from a sim's HTML
+ * @returns lowercase-kebab (repo-style) repo name (e.g. acid-base-solutions)
  */
-const getRepoNameFromStringKeyWithRepoName = stringKeyWithRepoName => {
+const getRepoNameFromStringKeyWithRepoName = ( stringKeyWithRepoName: string ): string => {
   logger.verbose( `getting lowercase-kebab repo name from ${stringKeyWithRepoName}` );
   const repoNameRegEx = /.*(?=\/)/;
   const repoName = stringKeyWithRepoName.match( repoNameRegEx );
+
+  if ( !repoName || repoName.length === 0 ) {
+    throw new Error( `Invalid stringKeyWithRepoName format: ${stringKeyWithRepoName}` );
+  }
+
   const lowercaseKebabRepoName = repoName[ 0 ].toLowerCase().replaceAll( '_', '-' );
   logger.verbose( `got lowercase-kebab repo name from ${stringKeyWithRepoName}; returning it` );
   return lowercaseKebabRepoName;
