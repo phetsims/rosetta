2ff5981afa3396ecfa2e2b6348d104e29d52aaf9
Sam Reid <reids@colorado.edu>
2025-04-16 09:52:18 -0600

    Move imports after file doc, see https://github.com/phetsims/perennial/issues/447

--- src/common/ClientSubmittedTranslationData.ts
+++ src/common/ClientSubmittedTranslationData.ts
@@ -1,13 +1,13 @@
 // Copyright 2025, University of Colorado Boulder
 
-import { TranslationFormData } from './TranslationFormData.js';
-
 /**
  * Type definition for the translation data that is sent from the client side to the server when the user submits a
  * translation for publication.  This defines the structure of the data that is essentially sent "over the wire".
  *
  * @author John Blanco (PhET Interactive Simulations)
  */
+
+import { TranslationFormData } from './TranslationFormData.js';
 
 type ClientSubmittedTranslationData = {
   locale: string;
