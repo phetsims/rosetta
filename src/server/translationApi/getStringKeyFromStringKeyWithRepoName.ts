27b5e2b1d0d9c7420ff6c9c64f7e3d3fb801bbb8
AgustinVallejo <agustin.vallejo@udea.edu.co>
2025-04-17 16:16:22 -0500

    continue conversion to TypeScript, see https://github.com/phetsims/rosetta/issues/311

--- src/server/translationApi/getStringKeyFromStringKeyWithRepoName.ts
+++ src/server/translationApi/getStringKeyFromStringKeyWithRepoName.ts
@@ -12,13 +12,13 @@ import logger from './logger.js';
  * Return a camelCase string key from a string that looks like stringKey/REPO_NAME. We get the strings that look like
  * REPO_NAME/stringKey from a sim's HTML.
  *
- * @param {String} stringKeyWithRepoName - stringKey/REPO_NAME we get from a sim's HTML
- * @returns {String} - camelCase string key
+ * @param stringKeyWithRepoName - stringKey/REPO_NAME we get from a sim's HTML
+ * @returns camelCase string key
  */
-const getStringKeyFromStringKeyWithRepoName = stringKeyWithRepoName => {
+const getStringKeyFromStringKeyWithRepoName = ( stringKeyWithRepoName: string ): string => {
   logger.verbose( `getting string key from ${stringKeyWithRepoName}` );
   logger.verbose( `got string key from ${stringKeyWithRepoName}; returning it` );
-  return stringKeyWithRepoName.match( /\/(.*)/ )[ 0 ].replace( '/', '' );
+  return stringKeyWithRepoName.match( /\/(.*)/ )![ 0 ].replace( '/', '' );
 };
 
 export default getStringKeyFromStringKeyWithRepoName;
