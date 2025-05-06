3300c74590c47a5509a6ab540c94615b5ec94f1f
Marla Schulz <marla.schulz@colorado.edu>
2025-05-06 11:11:12 -0700

    Refactor and generalize string migration script to support customizable targets and string keys, see: https://github.com/phetsims/center-and-variability/issues/624

--- scripts/js/move-string-from-repos-to-common.js
+++ scripts/js/move-string-from-repos-to-common.js
@@ -2,10 +2,14 @@
 
 /**
  * NodeJS script for moving a string that is repeated in several sims to a location in common code.  This was originally
- * created to move the "Projector Mode" string to joist, but might be reusable later.  See
+ * created to move the "Projector Mode" string to joist.  See
  * https://github.com/phetsims/joist/issues/342.
  *
+ * Run the script from Rosetta's root directory with:
+ * sage run rosetta/scripts/js/move-string-from-repos-to-common.js
+ *
  * @author John Blanco (PhET Interactive Simulations)
+ * @author Marla Schulz (PhET Interactive Simulations)
  */
 
 // modules
@@ -14,12 +18,12 @@ const _ = require( 'lodash' );
 
 // constants
 const SIMS_WITH_STRING = [
-  'charges-and-fields',
-  'gravity-and-orbits',
-  'molecule-shapes',
-  'rutherford-scattering',
-  'states-of-matter'
+  'your-sim',
+  'another-sim'
 ];
+
+const TARGET_REPO = 'target-common-repo';
+const STRING = 'stringKey';
 
 // helper function
 function extractLocaleFromFileName( fileName ) {
@@ -32,7 +36,7 @@ console.log( 'Starting move.' );
 
 SIMS_WITH_STRING.forEach( simName => {
 
-  const pathToStringsDir = '../../babel/' + simName;
+  const pathToStringsDir = '../babel/' + simName;
   const stringFileNameList = fs.readdirSync( pathToStringsDir );
 
   console.log( '================' );
@@ -46,22 +50,43 @@ SIMS_WITH_STRING.forEach( simName => {
 
     // look for a translation of the string and, if found, extract it and add it to the common code strings file
     _.keys( stringsObject ).forEach( stringKey => {
-      if ( stringKey.indexOf( 'projectorMode' ) >= 0 || stringKey.indexOf( 'projectorColors' ) >= 0 ) {
+      if ( stringKey.indexOf( STRING ) >= 0 ) {
         const locale = extractLocaleFromFileName( stringFileName );
         console.log( 'found translation for sim: ', simName, ', locale: ', locale );
 
         // find the joist string file for this locale
-        const joistStringFileName = '../../babel/joist/joist-strings_' + locale + '.json';
+        const targetStringFileName = `../babel/${TARGET_REPO}/${TARGET_REPO}-strings_` + locale + '.json';
 
-        // read in the joist string data
-        const joistStringsData = JSON.parse( fs.readFileSync( joistStringFileName, 'utf8' ) );
+       // If the file does not exist, we will create a new one.
+        if ( fs.existsSync( targetStringFileName ) ) {
 
-        // add in the new translated value
-        joistStringsData.projectorMode = stringsObject[ stringKey ];
+          // read in the joist string data
+          const stringsData = JSON.parse( fs.readFileSync( targetStringFileName, 'utf8' ) );
 
-        // write the modified string file
-        console.log( 'adding new string to file: ', joistStringFileName );
-        fs.writeFileSync( joistStringFileName, JSON.stringify( joistStringsData, null, 2 ) );
+          // Do not add translations that already exist in the target file.
+          if ( stringsData[ STRING ] ) {
+            console.log( 'WARNING: string already exists in translation file: ', targetStringFileName );
+            console.log( 'Reading next file without adding string.' );
+          }
+          else {
+            // add in the new translated value
+            stringsData[ STRING ] = stringsObject[ stringKey ];
+
+            // write the modified string file
+            console.log( 'adding new string to file: ', stringFileName );
+            fs.writeFileSync( targetStringFileName, JSON.stringify( stringsData, null, 2 ) );
+          }
+        }
+        else {
+          console.log( 'WARNING: target file does not exist: ', targetStringFileName );
+          const newStringsData = {
+            [ STRING ]: stringsObject[ stringKey ]
+          };
+
+          console.log( 'creating new file with string data:', newStringsData );
+          fs.writeFileSync( targetStringFileName, JSON.stringify( newStringsData, null, 2 ) );
+        }
+
       }
     } );
   } );
