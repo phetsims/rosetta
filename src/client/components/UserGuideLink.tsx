54f62074ee44e87390178c2920bfdb659002e72a
AgustinVallejo <agustin.vallejo@udea.edu.co>
2025-05-22 17:17:28 -0500

    Converting to tsx https://github.com/phetsims/rosetta/issues/311

--- src/client/components/UserGuideLink.tsx
+++ src/client/components/UserGuideLink.tsx
@@ -6,12 +6,12 @@
  * @author Liam Mulhall <liammulh@gmail.com>
  */
 
+import React, { ReactElement } from 'react';
+
 /**
  * Return a link to the user guide.
- *
- * @returns {JSX.Element}
  */
-const UserGuideLink = () => {
+const UserGuideLink = (): ReactElement => {
   return (
     <a target='_blank' rel='noreferrer' className='nav-link' href='https://docs.google.com/document/d/e/2PACX-1vSYc8f01StQ7e2nQWBA38BZfLoqkm6rkn-F9BzTmxdNgazOzFfLDm5RI-3I3IdKccuBFQpFdT2ST5Px/pub'>
       User Guide
