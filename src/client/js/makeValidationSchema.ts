2f577084b1e836da47fb96793e518728cd381005
AgustinVallejo <agustin.vallejo@udea.edu.co>
2025-05-23 12:19:17 -0500

    Converting js to ts https://github.com/phetsims/rosetta/issues/311

--- src/client/js/makeValidationSchema.ts
+++ src/client/js/makeValidationSchema.ts
@@ -12,25 +12,29 @@
 import * as Yup from 'yup';
 import { DOUBLE_BRACE_REGEX, SINGLE_BRACE_REGEX } from '../../common/constants';
 import KeyTypesEnum from '../../common/KeyTypesEnum';
+import { TranslationValues } from '../clientTypes';
 import alertErrorMessage from './alertErrorMessage';
 import isValidBracePattern from './isValidBracePattern';
 
+type ValidationSubObjects = {
+  [ keyType: string ]: {
+    [ stringKey: string ]: Yup.AnyObjectSchema;
+  };
+};
+
 /**
  * Return a Yup schema for validating our Formik translation form.
- *
- * @param {string} simRepoName - the repo name for the sim, e.g. acid-base-solutions
- * @param {Object} translationFormData - data we use to populate the translation form
- * @returns {Object} - the validation schema
  */
-const makeValidationSchema = ( simRepoName, translationFormData ) => {
-  const subObjects = {};
-  let validationSchema;
+const makeValidationSchema = (
+  simRepoName: string,
+  translationFormData: TranslationValues
+): Yup.AnyObjectSchema | { error: string } => {
+  const subObjects: ValidationSubObjects = {};
+  let validationSchema: Yup.AnyObjectSchema | { error: string };
 
   try {
-
     // We need to iterate over all types of keys.
     for ( const keyType of Object.values( KeyTypesEnum ) ) {
-
       // We have to replace dots in string keys with _DOT_. Otherwise, we might have errors related to the code think
       // there are more deeply nested objects than there really are.
       const stringKeys = Object.keys( translationFormData[ keyType ] ).map(
@@ -39,7 +43,6 @@ const makeValidationSchema = ( simRepoName, translationFormData ) => {
 
       subObjects[ keyType ] = {};
       for ( const stringKey of stringKeys ) {
-
         // If this is the sim title, add a validator indicating that it is required.
         if ( keyType === KeyTypesEnum.SIM_SPECIFIC && stringKey === `${simRepoName}_DOT_title` ) {
           subObjects[ keyType ][ stringKey ] = Yup.object( {
@@ -47,37 +50,38 @@ const makeValidationSchema = ( simRepoName, translationFormData ) => {
           } );
         }
         else {
-
           // Check if this string contains template patterns and, if so, add validation for them.
           const englishValue = translationFormData[ keyType ][ stringKey ].english;
           const englishSingleBraces = englishValue.match( SINGLE_BRACE_REGEX ) || [];
           const englishDoubleBraces = englishValue.match( DOUBLE_BRACE_REGEX ) || [];
           if ( englishSingleBraces.length > 0 || englishDoubleBraces.length > 0 ) {
-
             // We need to use a regular function here. Yup needs to be able to access the global "this" keyword, and if
-            // you use an anonymous arrow function, it can't do that. (Take this with a grain of salt, I (@liammulh) wrote
-            // this comment a while after I implemented this.)
+            // you use an anonymous arrow function, it can't do that.
             const placeholderErrorMessage = 'Curly brace pattern does not match English version.';
             subObjects[ keyType ][ stringKey ] = Yup.object( {
               translated: Yup.string().test( 'validBracePattern', placeholderErrorMessage, function foo( value ) {
-                return isValidBracePattern( value, englishValue );
+                return isValidBracePattern( value!, englishValue );
               } )
             } );
           }
         }
       }
     }
+
     const validationObject = {
-      simSpecific: Yup.object( subObjects.simSpecific ),
-      shared: Yup.object( subObjects.shared ),
-      common: Yup.object( subObjects.common )
+      simSpecific: Yup.object( subObjects.simSpecific || {} ),
+      shared: Yup.object( subObjects.shared || {} ),
+      common: Yup.object( subObjects.common || {} )
     };
+
     validationSchema = Yup.object().shape( validationObject );
   }
   catch( e ) {
-    alertErrorMessage( e );
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    alertErrorMessage( e as string );
     validationSchema = { error: 'unable to make validation schema' };
   }
+
   return validationSchema;
 };
 
