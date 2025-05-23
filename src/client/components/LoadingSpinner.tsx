bb66e4fc960b86ebff04ff94c83e427fcafd77ac
AgustinVallejo <agustin.vallejo@udea.edu.co>
2025-05-23 17:14:14 -0500

    Converting jsx to tsx https://github.com/phetsims/rosetta/issues/311

--- src/client/components/LoadingSpinner.tsx
+++ src/client/components/LoadingSpinner.tsx
@@ -6,12 +6,12 @@
  * @author Liam Mulhall <liammulh@gmail.com>
  */
 
+import React, { ReactElement } from 'react';
+
 /**
  * This component displays a loading "spinner" (a little circle that loops, indicating something is loading).
- *
- * @returns {JSX.Element}
  */
-const LoadingSpinner = () => {
+const LoadingSpinner = (): ReactElement => {
   return (
     <div className='spinner-border text-primary' role='status'>
       <span className='visually-hidden'>Loading...</span>
--- src/client/components/PhetHomePageLink.tsx
+++ src/client/components/PhetHomePageLink.tsx
@@ -6,12 +6,12 @@
  * @author Liam Mulhall <liammulh@gmail.com>
  */
 
+import React, { ReactElement } from 'react';
+
 /**
  * Return the link to the PhET home page.
- *
- * @returns {JSX.Element}
  */
-const PhetHomePageLink = () => {
+const PhetHomePageLink = (): ReactElement => {
   return <a className='nav-link' href={window.location.origin}>PhET Home Page</a>;
 };
 
